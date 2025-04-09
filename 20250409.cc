aeb094adb1c50d64c4df5e7608caf709b3ef9767
ROG STRIX <1820533976@qq.com>
2025-04-09 15:14:44 +0800

    #

--- 20250409.cc
+++ 20250409.cc
@@ -157,5 +157,18 @@ public:
 
     string modifyString(string s)
     {
+        int n = s.size();
+        for (int i = 0; i < n; i++)
+        {
+            if (s[i] == '?')
+            {
+                for (char ch = 'a'; ch <= 'z'; ch++)
+                {
+                    if ((i == 0 || s[i - 1] != ch) && (i == n - 1 || s[i + 1] != ch))
+                        s[i] = ch;
+                }
+            }
+        }
+        return s;
     }
 };
