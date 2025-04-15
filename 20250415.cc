942791487a7aac92d557dbc59bc237ca1c22ccd6
Kevin <you@example.com>
2025-04-16 00:49:52 +0800

    20250415

--- 20250415.cc
+++ 20250415.cc
@@ -10,6 +10,16 @@ struct ListNode
     ListNode(int x, ListNode *next) : val(x), next(next) {}
 };
 
+struct TreeNode
+{
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
 class Solution
 {
 public:
@@ -42,7 +52,127 @@ public:
         return prev;
     }
 
-    string longestPalindrome(string s) {
-        
+    string longestPalindrome(string s)
+    {
+    }
+
+    int tmp[50010];
+    int reversePairs(vector<int> &nums)
+    {
+        return mergeSort(nums, 0, nums.size() - 1);
+    }
+
+    int mergeSort(vector<int> &nums, int left, int right)
+    {
+        if (left >= right)
+            return 0;
+
+        int mid = left + (right - left) / 2;
+        int ret = 0;
+
+        ret += mergeSort(nums, left, mid);
+        ret += mergeSort(nums, mid + 1, right);
+
+        int cur1 = left, cur2 = mid + 1, i = 0;
+        while (cur1 <= mid && cur2 <= right)
+        {
+            if (nums[cur1] <= nums[cur2])
+                tmp[i++] = nums[cur1++];
+            else
+            {
+                ret += mid - cur1 + 1;
+                tmp[i++] = nums[cur2++];
+            }
+        }
+
+        while (cur1 <= mid)
+            tmp[i++] = nums[cur1++];
+        while (cur2 <= right)
+            tmp[i++] = nums[cur2++];
+
+        for (int j = left; j <= right; j++)
+            nums[j] = tmp[j - left];
+    }
+
+    vector<vector<int>> zigzagLevelOrder(TreeNode *root)
+    {
+        vector<vector<int>> ret;
+        if (root = nullptr)
+            return ret;
+
+        queue<TreeNode *> q;
+        q.push(root);
+
+        int level = 1;
+
+        while (q.size())
+        {
+            int sz = q.size();
+            vector<int> tmp;
+            for (int i = 0; i < sz; i++)
+            {
+                auto t = q.front();
+                q.pop();
+
+                tmp.push_back(t->val);
+                if (t->left)
+                    q.push(t->left);
+                if (t->right)
+                    q.push(t->right);
+            }
+
+            if (level % 2 == 0)
+                reverse(tmp.begin(), tmp.end());
+            ret.push_back(tmp);
+            level++;
+        }
+
+        return ret;
+    }
+
+    void reorderList(ListNode *head)
+    {
+        if (head == nullptr || head->next == nullptr || head->next->next == nullptr)
+            return;
+
+        ListNode *slow = head, *fast = head;
+        while (fast && fast->next)
+        {
+            slow = slow->next;
+            fast = fast->next->next;
+        }
+
+        ListNode *head2 = new ListNode(0);
+        head2->next = slow->next;
+        slow->next = nullptr;
+
+        ListNode *prev = head2, *cur = head2->next;
+        while (cur)
+        {
+            ListNode *next = cur->next;
+            prev->next = next;
+            next->next = cur;
+            cur = next;
+        }
+
+        ListNode *ret = new ListNode(0);
+        prev = ret;
+        ListNode *cur1 = head, *cur2 = head2->next;
+
+        while (cur1)
+        {
+            prev->next = cur1;
+            cur1 = cur1->next;
+            prev = prev->next;
+
+            if (cur2)
+            {
+                prev->next = cur2;
+                cur2 = cur2->next;
+                prev = prev->next;
+            }
+        }
+
+        return;
     }
 };
