26628b43276ee0bb892795130e51b12ecbd6ec49
Kevin <kutbass@163.com>
2025-04-21 10:52:26 +0800

    20250421

---
+++ 20250421.cc
@@ -0,0 +1,431 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+struct ListNode
+{
+    int val;
+    ListNode *next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
+
+struct TreeNode
+{
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+class Solution
+{
+public:
+    int tmp[50010];
+    int reversePairs(vector<int> &nums)
+    {
+        return mergeSort(nums, 0, nums.size() - 1);
+    }
+
+    int mergeSort(vector<int> &nums, int left, int right)
+    {
+        if (left >= right)
+            return 0;
+
+        int mid = left + (right - left) / 2;
+        int ret = 0;
+        ret += mergeSort(nums, left, mid);
+        ret += mergeSort(nums, mid + 1, right);
+
+        int i = 0, cur1 = left, cur2 = mid + 1;
+        while (cur1 <= mid)
+        {
+            while (cur2 <= right && nums[cur1] <= 2.0 * nums[cur2])
+                cur2++;
+
+            if (cur2 >= right)
+                break;
+
+            ret += right - cur2 + 1;
+            cur1++;
+        }
+
+        cur1 = left, cur2 = mid + 1;
+        while (cur1 <= mid && cur2 <= right)
+            tmp[i++] = nums[cur1] <= nums[cur2] ? nums[cur2++] : nums[cur1++];
+        while (cur1 <= mid)
+            tmp[i++] = nums[cur1++];
+        while (cur2 <= right)
+            tmp[i++] = nums[cur2++];
+        for (int j = left; j <= right; j++)
+            nums[j] = tmp[j - left];
+        return ret;
+    }
+
+    ListNode *swapPairs(ListNode *head)
+    {
+        if (head == nullptr || head->next == nullptr)
+            return head;
+
+        ListNode *newhead = new ListNode(0);
+        ListNode *prev = newhead;
+        ListNode *cur = head, *next = cur->next, *nnext = next->next;
+
+        while (cur && next)
+        {
+            prev->next = next;
+            next->next = cur;
+            cur->next = nnext;
+
+            prev = cur;
+            cur = nnext;
+            if (cur)
+                next = cur->next;
+            if (next)
+                nnext = next->next;
+        }
+
+        prev = newhead->next;
+        delete newhead;
+        return prev;
+    }
+
+    string addBinary(string a, string b)
+    {
+        int m = a.size(), n = b.size();
+        string ret;
+
+        int cur1 = m - 1, cur2 = n - 1, t = 0;
+        while (cur1 >= 0 || cur2 >= 0 || t)
+        {
+            if (cur1 >= 0)
+                t += (a[cur1--] - '0');
+            if (cur2 >= 0)
+                t += (b[cur2--] - '0');
+            ret += (t % 2 + '0');
+            t /= 2;
+        }
+
+        reverse(ret.begin(), ret.end());
+        return ret;
+    }
+
+    int tmp[50010];
+    int reversePairs(vector<int> &nums)
+    {
+        return mergesort(nums, 0, nums.size() - 1);
+    }
+
+    int mergesort(vector<int> &nums, int left, int right)
+    {
+        if (left >= right)
+            return 0;
+
+        int mid = left + (right - left) / 2;
+        int ret = 0;
+        ret += mergesort(nums, left, mid);
+        ret += mergesort(nums, mid + 1, right);
+
+        int i = 0, cur1 = left, cur2 = mid + 1;
+        while (cur1 <= mid && cur2 <= right)
+        {
+            if (nums[cur1] <= nums[cur2])
+                tmp[i++] = nums[cur2++];
+            else
+            {
+                ret += right - cur2 + 1;
+                tmp[i++] = nums[cur1++];
+            }
+        }
+
+        while (cur1 <= mid)
+            tmp[i++] = nums[cur1++];
+        while (cur2 <= right)
+            tmp[i++] = nums[cur2++];
+        for (int j = left; j <= right; j++)
+            nums[j] = tmp[j - left];
+        return ret;
+    }
+
+    int waysToStep(int n)
+    {
+        if (n < 3)
+            return n;
+
+        int base = 1000000007, dp1 = 1, dp2 = 2, dp3 = 4, tmp2, tmp3;
+        while (n-- != 3)
+        {
+            tmp3 = dp3, tmp2 = dp2;
+            dp3 = ((dp3 + dp2) % base + dp1) % base;
+            dp2 = tmp3, dp1 = tmp2;
+        }
+        return dp3;
+    }
+
+    void reorderList(ListNode *head)
+    {
+        if (head == nullptr || head->next == nullptr || head->next->next == nullptr)
+            return;
+
+        ListNode *slow = head, *fast = head;
+        while (fast && fast->next)
+        {
+            slow = slow->next;
+            fast = fast->next->next;
+        }
+
+        ListNode *head2 = new ListNode(0);
+        ListNode *cur = slow->next;
+        slow->next = nullptr;
+
+        while (cur)
+        {
+            // _prev->next = cur;
+            // cur->next = _prev->next;
+
+            // cur = cur->next;
+            ListNode *next = cur->next;
+            cur->next = head2->next;
+            head2->next = cur;
+
+            cur = next;
+        }
+
+        ListNode *ret = new ListNode(0);
+        ListNode *prev = ret;
+        ListNode *cur1 = head, *cur2 = head2->next;
+
+        while (cur1)
+        {
+            prev->next = cur1;
+            prev = prev->next;
+            cur1 = cur1->next;
+
+            if (cur2)
+            {
+                prev->next = cur2;
+                prev = prev->next;
+                cur2 = cur2->next;
+            }
+        }
+
+        // head = ret->next;
+        delete ret, head2;
+        return;
+    }
+
+    ListNode *mergeKLists(vector<ListNode *> &lists)
+    {
+        return mergeLists(lists, 0, lists.size() - 1);
+    }
+
+    ListNode *mergeLists(vector<ListNode *> &lists, int left, int right)
+    {
+        if (left > right)
+            return nullptr;
+        if (left == right)
+            return lists[left];
+
+        int mid = left + (right - left) / 2;
+        ListNode *l1 = mergeLists(lists, left, mid);
+        ListNode *l2 = mergeLists(lists, mid + 1, right);
+
+        return merge2Lists(l1, l2);
+    }
+
+    ListNode *merge2Lists(ListNode *l1, ListNode *l2)
+    {
+        if (l1 == nullptr)
+            return l2;
+        if (l2 == nullptr)
+            return l1;
+
+        ListNode *head = new ListNode(0);
+        ListNode *prev = head;
+        ListNode *cur1 = l1, *cur2 = l2;
+
+        while (cur1 && cur2)
+        {
+            if (cur1->val <= cur2->val)
+            {
+                prev->next = cur1;
+                prev = prev->next;
+                cur1 = cur1->next;
+            }
+            else
+            {
+                prev->next = cur2;
+                prev = prev->next;
+                cur2 = cur2->next;
+            }
+        }
+
+        if (cur1)
+            prev->next = cur1;
+        if (cur2)
+            prev->next = cur2;
+
+        prev = head->next;
+        delete head;
+        return prev;
+    }
+
+    vector<int> ret;
+    vector<int> tmpNums;
+    vector<int> index;
+    vector<int> tmpIndex;
+    vector<int> countSmaller(vector<int> &nums)
+    {
+        int n = nums.size();
+        ret.resize(n);
+        tmpNums.resize(n);
+        index.resize(n);
+        tmpIndex.resize(n);
+        for (int i = 0; i < n; i++)
+            index[i] = i;
+        merge_sort(nums, 0, n - 1);
+        return ret;
+    }
+
+    void merge_sort(vector<int> &nums, int left, int right)
+    {
+        if (left >= right)
+            return;
+
+        int mid = left + (right - left) / 2;
+        merge_sort(nums, left, mid);
+        merge_sort(nums, mid + 1, right);
+
+        int i = 0, cur1 = left, cur2 = mid + 1;
+        while (cur1 <= mid && cur2 <= right)
+        {
+            if (nums[cur1] <= nums[cur2])
+            {
+                tmpNums[i] = nums[cur2];
+                tmpIndex[i++] = index[cur2++];
+            }
+            else
+            {
+                ret[index[cur1]] += right - cur2 + 1;
+                tmpNums[i] = nums[cur1];
+                tmpIndex[i++] = index[cur1++];
+            }
+        }
+
+        while (cur1 <= mid)
+        {
+            tmpNums[i] = nums[cur1];
+            tmpIndex[i++] = index[cur1++];
+        }
+        while (cur2 <= right)
+        {
+            tmpNums[i] = nums[cur2];
+            tmpIndex[i++] = index[cur2++];
+        }
+        for (int j = left; j <= right; j++)
+        {
+            nums[j] = tmpNums[j - left];
+            index[j] = tmpIndex[j - left];
+        }
+        return;
+    }
+
+    int calculate(string s)
+    {
+        vector<int> tmp;
+        char op = '+';
+
+        for (int i = 0; i < s.size();)
+        {
+            if (s[i] == ' ')
+            {
+                i++;
+                continue;
+            }
+            else if (s[i] >= '0' && s[i] <= '9')
+            {
+                int t = 0;
+                while (i < s.size() && s[i] >= '0' && s[i] <= '9')
+                    t = t * 10 + (s[i++] - '0');
+                if (op == '+')
+                    tmp.push_back(t);
+                else if (op == '-')
+                    tmp.push_back(-t);
+                else if (op == '*')
+                    tmp.back() *= t;
+                else
+                    tmp.back() /= t;
+            }
+            else
+                op = s[i++];
+        }
+
+        int ret = 0;
+        for (auto n : tmp)
+            ret += n;
+        return ret;
+    }
+
+    string changeStr(string s)
+    {
+        string ret;
+        for (auto ch : s)
+        {
+            if (ch != '#')
+                ret += ch;
+            else if (ret.size())
+                ret.pop_back();
+        }
+        return ret;
+    }
+
+    bool backspaceCompare(string s, string t)
+    {
+        return changeStr(s) == changeStr(t);
+    }
+
+    int dx[4] = {1, -1, 0, 0};
+    int dy[4] = {0, 0, 1, -1};
+    bool vis[301][301];
+    int m, n;
+    int numIslands(vector<vector<char>> &grid)
+    {
+        m = grid.size(), n = grid[0].size();
+        int ret = 0;
+        for (int i = 0; i < m; i++)
+            for (int j = 0; j < n; j++)
+            {
+                if (grid[i][j] == '1' && !vis[i][j])
+                {
+                    ret++;
+                    bfs(grid, i, j);
+                }
+            }
+
+        return ret;
+    }
+
+    void bfs(vector<vector<char>> &grid, int i, int j)
+    {
+        queue<pair<int, int>> q;
+        q.push({i, j});
+        vis[i][j] = true;
+
+        while (q.size())
+        {
+            auto [a, b] = q.front();
+            q.pop();
+            for (int k = 0; k < 4; k++)
+            {
+                int x = a + dx[k], y = b + dy[k];
+                if (x >= 0 && x < m && y >= 0 && y < n && grid[x][y] == '1' && !vis[x][y])
+                {
+                    q.push({x, y});
+                    vis[x][y] = true;
+                }
+            }
+        }
+    }
+};
