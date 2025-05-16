17d744c8fb077ff3cc5ea8df01dbd97f76330856
Kevin <kutbass@163.com>
2025-05-16 08:56:00 +0800

    20250516

--- 20250516.cc
+++ 20250516.cc
@@ -273,6 +273,35 @@ public:
 
     ListNode *reverseKGroup(ListNode *head, int k)
     {
-        
+        int n = 0;
+        ListNode *cur = head;
+        while (cur)
+        {
+            cur = cur->next;
+            n++;
+        }
+        n /= k;
+
+        ListNode *newhead = new ListNode(0);
+        ListNode *prev = newhead;
+        cur = head;
+
+        for (int i = 0; i < n; i++)
+        {
+            ListNode *tmp = cur;
+            for (int j = 0; j < k; j++)
+            {
+                ListNode *next = cur->next;
+                cur->next = prev->next;
+                prev->next = cur;
+                cur = next;
+            }
+            prev = tmp;
+        }
+
+        prev->next = cur;
+        cur = newhead->next;
+        delete newhead;
+        return cur;
     }
 };
