9e9bb4bc54af56696690e8524a270de95bf163f5
ROG STRIX <1820533976@qq.com>
2025-05-22 12:25:51 +0800

    20250522

--- 20250522.cc
+++ 20250522.cc
@@ -20,6 +20,26 @@ struct TreeNode
     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
 };
 
+class Node
+{
+public:
+    int val;
+    vector<Node *> children;
+
+    Node() {}
+
+    Node(int _val)
+    {
+        val = _val;
+    }
+
+    Node(int _val, vector<Node *> _children)
+    {
+        val = _val;
+        children = _children;
+    }
+};
+
 class Solution
 {
 public:
@@ -133,6 +153,127 @@ public:
             prev = prev->next;
             if (t->next)
                 q.push(t->next);
+        }
+
+        prev = head->next;
+        delete head;
+        return prev;
+    }
+
+    int widthOfBinaryTree(TreeNode *root)
+    {
+        vector<pair<TreeNode *, unsigned int>> q;
+        q.push_back({root, 1});
+
+        unsigned int ret = 0;
+
+        while (q.size())
+        {
+            auto &[x1, y1] = q[0];
+            auto &[x2, y2] = q.back();
+            ret = max(ret, y2 - y1 + 1);
+
+            vector<pair<TreeNode *, unsigned int>> tmp;
+            for (auto &[x, y] : q)
+            {
+                if (x->left)
+                    tmp.push_back({x->left, 2 * y});
+                if (x->right)
+                    tmp.push_back({x->right, 2 * y + 1});
+            }
+
+            q = tmp;
+        }
+
+        return ret;
+    }
+
+    vector<vector<int>> levelOrder(Node *root)
+    {
+        vector<vector<int>> ret;
+        if (root == nullptr)
+            return ret;
+
+        queue<Node *> q;
+        q.push(root);
+
+        while (q.size())
+        {
+            int sz = q.size();
+            vector<int> tmp;
+            while (sz--)
+            {
+                auto t = q.front();
+                q.pop();
+                tmp.push_back(t->val);
+
+                for (auto node : t->children)
+                    if (node)
+                        q.push(node);
+            }
+            ret.push_back(tmp);
+        }
+
+        return ret;
+    }
+
+    int widthOfBinaryTree(TreeNode *root)
+    {
+        vector<pair<TreeNode *, unsigned int>> q;
+        q.push_back({root, 1});
+
+        unsigned int ret = 0;
+        while (q.size())
+        {
+            auto &[x1, y1] = q[0];
+            auto &[x2, y2] = q.back();
+            ret = max(ret, y2 - y1 + 1);
+
+            vector<pair<TreeNode *, unsigned int>> tmp;
+            for (auto &[x, y] : q)
+            {
+                if (x->left)
+                    tmp.push_back({x->left, 2 * y});
+                if (x->right)
+                    tmp.push_back({x->right, 2 * y + 1});
+            }
+
+            q = tmp;
+        }
+
+        return ret;
+    }
+
+    struct cmp
+    {
+        bool operator()(const ListNode *l1, const ListNode *l2)
+        {
+            return l1->val > l2->val;
+        }
+    };
+
+    ListNode *mergeKLists(vector<ListNode *> &lists)
+    {
+        priority_queue<ListNode *, vector<ListNode *>, cmp> q;
+        for (auto &l : lists)
+            if (l)
+                q.push(l);
+
+        ListNode *head = new ListNode(0);
+        ListNode *prev = head;
+        while (q.size())
+        {
+            int sz = q.size();
+            while (sz--)
+            {
+                auto t = q.top();
+                q.pop();
+                prev->next = t;
+                prev = prev->next;
+
+                if (t->next)
+                    q.push(t->next);
+            }
         }
 
         prev = head->next;
