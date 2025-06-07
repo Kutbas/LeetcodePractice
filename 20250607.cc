96916bf520914247e46ed1377bfc838a5369cd78
ROG STRIX <1820533976@qq.com>
2025-06-07 15:45:17 +0800

    20250607

---
+++ 20250607.cc
@@ -0,0 +1,207 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+struct ListNode
+{
+    int val;
+    ListNode *next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
+
+struct TreeNode
+{
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+class Solution
+{
+public:
+    int hammingDistance(int x, int y)
+    {
+        int ret = 0;
+        for (int i = 0; i < 32; i++)
+        {
+            int a = (x >> i) & 1;
+            int b = (y >> i) & 1;
+            if (a != b)
+                ret++;
+        }
+        return ret;
+    }
+
+    ListNode *reverseKGroup(ListNode *head, int k)
+    {
+        int n = 0;
+        ListNode *cur = head;
+        while (cur)
+        {
+            cur = cur->next;
+            n++;
+        }
+
+        n /= k;
+
+        ListNode *newhead = new ListNode(0);
+        ListNode *prev = newhead;
+        cur = head;
+
+        for (int i = 0; i < n; i++)
+        {
+            ListNode *tmp = cur;
+            for (int j = 0; j < k; j++)
+            {
+                ListNode *next = cur->next;
+                cur->next = prev->next;
+                prev->next = cur;
+                cur = next;
+            }
+            prev = tmp;
+        }
+
+        prev->next = cur;
+        prev = newhead->next;
+        delete newhead;
+        return prev;
+    }
+
+    int dx[4] = {0, 0, 1, -1};
+    int dy[4] = {1, -1, 0, 0};
+    int m, n;
+    vector<vector<int>> updateMatrix(vector<vector<int>> &mat)
+    {
+        m = mat.size(), n = mat[0].size();
+        vector<vector<int>> dist(m, vector<int>(n, -1));
+        queue<pair<int, int>> q;
+        for (int i = 0; i < m; i++)
+            for (int j = 0; j < n; j++)
+                if (mat[i][j] == 0)
+                {
+                    dist[i][j] = 0;
+                    q.push({i, j});
+                }
+
+        while (q.size())
+        {
+            auto [a, b] = q.front();
+            q.pop();
+            for (int k = 0; k < 4; k++)
+            {
+                int x = a + dx[k], y = b + dy[k];
+                if (x >= 0 && x < m && y >= 0 && y < n && dist[x][y] == -1)
+                {
+                    dist[x][y] = dist[a][b] + 1;
+                    q.push({x, y});
+                }
+            }
+        }
+
+        return dist;
+    }
+
+    string decodeString(string s)
+    {
+        string ret;
+        stack<string> st;
+        stack<int> nums;
+        int k = 0;
+
+        for (char ch : s)
+        {
+            if (isdigit(ch))
+                k = k * 10 + ch - '0';
+            else if (ch == '[')
+            {
+                nums.push(k);
+                k = 0;
+                st.push(ret);
+                ret = "";
+            }
+            else if (ch == ']')
+            {
+                int n = nums.top();
+                nums.pop();
+                string tmp = st.top();
+                st.pop();
+                while (n--)
+                    tmp += ret;
+                ret = tmp;
+            }
+            else
+                ret += ch;
+        }
+
+        return ret;
+    }
+
+    unordered_map<char, unordered_set<char>> hash;
+    unordered_map<char, int> in;
+    bool check = false;
+    string alienOrder(vector<string> &words)
+    {
+        int n = words.size();
+        for (auto word : words)
+            for (auto ch : word)
+                in[ch] = 0;
+
+        for (int i = 0; i < n; i++)
+            for (int j = i + 1; j < n; j++)
+            {
+                add(words[i], words[j]);
+                if (check)
+                    return "";
+            }
+
+        string ret;
+        queue<char> q;
+        for (auto [a, b] : in)
+            if (b == 0)
+                q.push(a);
+
+        while (q.size())
+        {
+            auto t = q.front();
+            q.pop();
+            ret += t;
+
+            for (auto a : hash[t])
+                if (--in[a] == 0)
+                    q.push(a);
+        }
+
+        for (auto [a, b] : in)
+            if (b)
+                return "";
+
+        return ret;
+    }
+
+    void add(string &s1, string &s2)
+    {
+        int n = min(s1.size(), s2.size());
+        int i = 0;
+        for (; i < n; i++)
+        {
+            if (s1[i] != s2[i])
+            {
+                char a = s1[i], b = s2[i]; // a->b
+                if (!hash.count(a) || !hash[a].count(b))
+                {
+                    hash[a].insert(b);
+                    in[b]++;
+                }
+                break;
+            }
+        }
+
+        // abc ab
+        if (i == s2.size() && i < s1.size())
+            check = true;
+    }
+};
