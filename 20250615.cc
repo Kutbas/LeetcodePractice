89048a947d2a0b0d2d5ac9d04f4f0dbfed0339d5
ROG STRIX <1820533976@qq.com>
2025-06-15 14:43:18 +0800

    20250615

---
+++ 20250615.cc
@@ -0,0 +1,280 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+struct ListNode
+{
+    int val;
+    ListNode *next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
+
+struct TreeNode
+{
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+class Solution
+{
+public:
+    int subarraysDivByK(vector<int> &nums, int k)
+    {
+        unordered_map<int, int> hash;
+        int ret = 0, sum = 0;
+        hash[0] = 1;
+
+        for (auto n : nums)
+        {
+            sum += n;
+            int r = (sum % k + k) % k;
+            if (hash.count(r))
+                ret += hash[r];
+            hash[r]++;
+        }
+
+        return ret;
+    }
+
+    TreeNode *pruneTree(TreeNode *root)
+    {
+        if (root == nullptr)
+            return nullptr;
+
+        root->left = pruneTree(root->left);
+        root->right = pruneTree(root->right);
+
+        if (root->left == nullptr && root->right == nullptr && root->val == 0)
+            return nullptr;
+
+        return root;
+    }
+
+    string minWindow(string s, string t)
+    {
+        int hash1[128] = {0};
+        int hash2[128] = {0};
+        int m = t.size(), begin = -1, minlen = INT_MAX;
+
+        for (char ch : t)
+            hash1[ch]++;
+
+        for (int left = 0, right = 0, count = 0; right < s.size(); right++)
+        {
+            char in = s[right];
+            if (++hash2[in] <= hash1[in])
+                count++;
+            while (count == m)
+            {
+                if (right - left + 1 < minlen)
+                {
+                    minlen = right - left + 1;
+                    begin = left;
+                }
+                char out = s[left++];
+                if (hash2[out]-- <= hash1[out])
+                    count--;
+            }
+        }
+
+        return begin == -1 ? "" : s.substr(begin, minlen);
+    }
+
+    vector<int> singleNumber(vector<int> &nums)
+    {
+        int xorsum = 0;
+        for (auto n : nums)
+            xorsum ^= n;
+
+        int lsb = xorsum == INT_MIN ? xorsum : xorsum & (-xorsum);
+        int x = 0, y = 0;
+
+        for (auto n : nums)
+        {
+            if (n & lsb)
+                x ^= n;
+            else
+                y ^= n;
+        }
+
+        return {x, y};
+    }
+
+    unordered_map<char, unordered_set<char>> hash;
+    unordered_map<char, int> in;
+    bool check = false;
+    string alienOrder(vector<string> &words)
+    {
+        for (auto word : words)
+            for (auto ch : word)
+                in[ch] = 0;
+
+        for (int i = 0; i < words.size(); i++)
+            for (int j = i + 1; j < words.size(); j++)
+            {
+                add(words[i], words[j]);
+
+                if (check)
+                    return "";
+            }
+
+        string ret;
+        queue<char> q;
+        for (auto [a, b] : in)
+            if (b == 0)
+                q.push(a);
+
+        while (q.size())
+        {
+            auto t = q.front();
+            q.pop();
+            ret += t;
+
+            for (auto a : hash[t])
+                if (--in[a] == 0)
+                    q.push(a);
+        }
+
+        for (auto [a, b] : in)
+            if (b != 0)
+                return "";
+
+        return ret;
+    }
+
+    void add(string &s1, string &s2)
+    {
+        int n = min(s1.size(), s2.size());
+        int i = 0;
+        for (; i < n; i++)
+        {
+            if (s1[i] != s2[i])
+            {
+                char a = s1[i], b = s2[i]; // wrt wrf t->f
+                if (!hash.count(a) || !hash[a].count(b))
+                {
+                    hash[a].insert(b);
+                    in[b]++;
+                }
+                break;
+            }
+        }
+
+        // abc ab
+        if (i < s1.size() && i == s2.size())
+            check = true;
+    }
+
+    TreeNode *pruneTree(TreeNode *root)
+    {
+        if (root == nullptr)
+            return nullptr;
+
+        root->left = pruneTree(root->left);
+        root->right = pruneTree(root->right);
+        if (root->left == nullptr && root->right == nullptr && root->val == 0)
+            return nullptr;
+
+        return root;
+    }
+
+    int ret, count;
+    int kthSmallest(TreeNode *root, int k)
+    {
+        count = k;
+        dfs(root);
+        return ret;
+    }
+
+    void dfs(TreeNode *root)
+    {
+        if (root == nullptr || count == 0)
+            return;
+
+        dfs(root->left);
+        if (--count == 0)
+            ret = root->val;
+        dfs(root->right);
+    }
+
+    vector<int> tmp;
+    vector<int> index;
+    vector<int> tmpIndex;
+    vector<int> Ret;
+
+    vector<int> countSmaller(vector<int> &nums)
+    {
+        int n = nums.size();
+        tmp.resize(n);
+        index.resize(n);
+        tmpIndex.resize(n);
+        Ret.resize(n);
+
+        for (int i = 0; i < n; i++)
+            index[i] = i;
+
+        mergeSort(nums, 0, nums.size() - 1);
+
+        return Ret;
+    }
+
+    void mergeSort(vector<int> &nums, int left, int right)
+    {
+        if (left >= right)
+            return;
+
+        int mid = left + (right - left) / 2;
+        mergeSort(nums, left, mid);
+        mergeSort(nums, mid + 1, right);
+
+        int i = 0, cur1 = left, cur2 = mid + 1;
+        while (cur1 <= mid && cur2 <= right)
+        {
+            if (nums[cur1] <= nums[cur2])
+            {
+                tmp[i] = nums[cur2];
+                tmpIndex[i++] = index[cur2++];
+            }
+            else
+            {
+                Ret[index[cur1]] += right - cur2 + 1;
+                tmp[i] = nums[cur1];
+                tmpIndex[i++] = index[cur1++];
+            }
+        }
+
+        while (cur1 <= mid)
+        {
+            tmp[i] = nums[cur1];
+            tmpIndex[i++] = index[cur1++];
+        }
+        while (cur2 <= right)
+        {
+            tmp[i] = nums[cur2];
+            tmpIndex[i++] = index[cur2++];
+        }
+
+        for (int j = left; j <= right; j++)
+        {
+            nums[j] = tmp[j - left];
+            index[j] = tmpIndex[j - left];
+        }
+    }
+
+    TreeNode *pruneTree(TreeNode *root)
+    {
+        if (root == nullptr)
+            return nullptr;
+
+        root->left = pruneTree(root->left);
+        root->right = pruneTree(root->right);
+        if (root->left == nullptr && root->right == nullptr && root->val == 0)
+            return nullptr;
+        return root;
+    }
+};
