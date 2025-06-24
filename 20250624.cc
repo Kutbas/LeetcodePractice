f754ce58e8a997c4a6c996bf0e32635f00d50eb4
ROG STRIX <1820533976@qq.com>
2025-06-24 22:31:01 +0800

    20250624

---
+++ 20250624.cc
@@ -0,0 +1,235 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+struct ListNode
+{
+    int val;
+    ListNode *next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
+
+struct TreeNode
+{
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+class Solution
+{
+public:
+    int subarraySum(vector<int> &nums, int k)
+    {
+        unordered_map<int, int> hash;
+        int ret = 0, sum = 0;
+        hash[0] = 1;
+
+        for (auto n : nums)
+        {
+            sum += n;
+            if (hash.count(sum - k))
+                ret += hash[sum - k];
+            hash[sum]++;
+        }
+
+        return ret;
+    }
+
+    vector<vector<int>> ret;
+    vector<int> path;
+    vector<vector<int>> subsets(vector<int> &nums)
+    {
+        dfs(nums, 0);
+        return ret;
+    }
+
+    void dfs(vector<int> &nums, int pos)
+    {
+        if (pos == nums.size())
+            return;
+        ret.push_back(path);
+
+        for (int i = pos; i < nums.size(); i++)
+        {
+            path.push_back(nums[i]);
+            dfs(nums, pos + 1);
+            path.pop_back();
+        }
+    }
+
+    unordered_map<char, unordered_set<int>> hash;
+    unordered_map<char, int> in;
+    bool check = false;
+    string alienOrder(vector<string> &words)
+    {
+        for (auto &word : words)
+            for (auto &ch : word)
+                in[ch] = 0;
+
+        for (int i = 0; i < words.size(); i++)
+            for (int j = i + 1; j < words.size(); j++)
+            {
+                add(words[i], words[j]);
+                if (check)
+                    return "";
+            }
+
+        string ret;
+        queue<char> q;
+        for (auto [a, b] : in)
+            if (b == 0)
+                q.push(a);
+
+        while (q.size())
+        {
+            auto t = q.front();
+            q.pop();
+            ret += t;
+
+            for (auto a : hash[t])
+                if (--in[a] == 0)
+                    q.push(a);
+        }
+
+        for (auto [a, b] : in)
+            if (b)
+                return "";
+        return ret;
+    }
+
+    void add(string &s1, string &s2)
+    {
+        int n = min(s1.size(), s2.size());
+        int i = 0;
+        for (; i < n; i++)
+        {
+            if (s1[i] != s2[i])
+            {
+                char a = s1[i], b = s2[i]; // wrt wrf -> in[a]++
+                if (!hash.count(b) || !hash[b].count(a))
+                {
+                    hash[b].insert(a);
+                    in[a]++;
+                }
+                break;
+            }
+        }
+
+        // abc ab
+        if (n < s1.size() && n == s2.size())
+            check = true;
+    }
+
+    vector<int> smallestK(vector<int> &arr, int k)
+    {
+        srand(time(nullptr));
+        quickSort(arr, 0, arr.size() - 1, k);
+        return {arr.begin(), arr.begin() + k};
+    }
+
+    int getRandom(vector<int> &arr, int left, int right)
+    {
+        int r = rand();
+        return arr[r % (right - left + 1) + left];
+    }
+
+    void quickSort(vector<int> &nums, int l, int r, int k)
+    {
+        if (l >= r)
+            return;
+
+        int key = getRandom(nums, l, r);
+        int i = l, left = l - 1, right = r + 1;
+
+        while (i < right)
+        {
+            if (nums[i] < key)
+                swap(nums[i++], nums[++left]);
+            else if (nums[i] == key)
+                i++;
+            else
+                swap(nums[i], nums[--right]);
+        }
+
+        int lRange = left - l + 1, mRange = (right - 1) - (left + 1) + 1;
+        if (lRange >= k)
+            quickSort(nums, l, left, k);
+        else if (lRange + mRange >= k)
+            return;
+        else
+            quickSort(nums, right, r, k - lRange - mRange);
+    }
+
+    void hanota(vector<int> &A, vector<int> &B, vector<int> &C)
+    {
+        dfs(A, B, C, A.size());
+    }
+
+    void dfs(vector<int> &from, vector<int> &buffer, vector<int> &to, int n)
+    {
+        if (n == 1)
+        {
+            to.push_back(from.back());
+            from.pop_back();
+            return;
+        }
+
+        dfs(from, to, buffer, n - 1);
+        to.push_back(from.back());
+        from.pop_back();
+        dfs(buffer, from, to, n - 1);
+    }
+
+    bool vis[21][21];
+    int dx[4] = {1, -1, 0, 0};
+    int dy[4] = {0, 0, 1, -1};
+    int ret;
+    int m, n, step;
+    int uniquePathsIII(vector<vector<int>> &grid)
+    {
+        m = grid.size(), n = grid[0].size();
+
+        int bx = 0, by = 0;
+        for (int i = 0; i < m; i++)
+            for (int j = 0; j < n; j++)
+                if (grid[i][j] == 0)
+                    step++;
+                else if (grid[i][j] == 1)
+                {
+                    bx = i;
+                    by = j;
+                }
+
+        step += 2;
+
+        vis[bx][by] = true;
+        dfs(grid, bx, by, 1);
+        return ret;
+    }
+
+    void dfs(vector<vector<int>> &grid, int i, int j, int count)
+    {
+        if (grid[i][j] == 2)
+        {
+            if (count == step)
+                ret++;
+            return;
+        }
+
+        for (int k = 0; k < 4; k++)
+        {
+            int x = i + dx[k], y = j + dy[k];
+            if (x >= 0 && x < m && y >= 0 && y < n && !vis[x][y] && grid[x][y] != -1)
+            {
+                vis[x][y] = true;
+                dfs(grid, x, y, count + 1);
+                vis[x][y] = false;
+            }
+        }
+    }
+};
