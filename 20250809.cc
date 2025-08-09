2c22e6e982890c872eb4af0ddc0567ddefe5d5d2
ROG STRIX <1820533976@qq.com>
2025-08-09 20:27:00 +0800

    20250809

--- 20250809.cc
+++ 20250809.cc
@@ -247,4 +247,30 @@ public:
             }
         }
     }
+
+    // 8
+    vector<int> advantageCount(vector<int> &nums1, vector<int> &nums2)
+    {
+        int n = nums1.size();
+        sort(nums1.begin(), nums1.end());
+
+        vector<int> index2(n);
+        for (int i = 0; i < n; i++)
+            index2[i] = i;
+
+        sort(index2.begin(), index2.end(), [&](int i, int j)
+             { return nums2[i] < nums2[j]; });
+
+        vector<int> ret(n);
+        int left = 0, right = n - 1;
+        for (auto x : nums1)
+        {
+            if (x > nums2[index2[left]])
+                ret[index2[left++]] = x;
+            else
+                ret[index2[right--]] = x;
+        }
+
+        return ret;
+    }
 };
