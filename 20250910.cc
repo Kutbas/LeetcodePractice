1df93c4e1aa6a44bf30c088057b7d56889b6a794
Kevin <kutbass@163.com>
2025-09-10 09:57:29 +0800

    20250910

---
+++ 20250910.cc
@@ -0,0 +1,261 @@
+#include <bits/stdc++.h>
+#include <mutex>
+using namespace std;
+
+struct ListNode
+{
+    int val;
+    ListNode *next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
+
+struct TreeNode
+{
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+class Solution
+{
+public:
+    // 1
+    struct cmp
+    {
+        bool operator()(const ListNode *l1, const ListNode *l2)
+        {
+            return l1->val < l2->val;
+        }
+    };
+
+    ListNode *mergeKLists(vector<ListNode *> &lists)
+    {
+        priority_queue<ListNode *, vector<ListNode *>, cmp> q;
+        for (auto &list : lists)
+            if (list)
+                q.push(list);
+
+        ListNode *ret = new ListNode(0);
+        ListNode *prev = ret;
+
+        while (q.size())
+        {
+            auto t = q.top();
+            q.pop();
+
+            prev->next = t;
+            prev = prev->next;
+
+            if (t->next)
+                q.push(t->next);
+        }
+
+        prev = ret->next;
+        delete ret;
+        return prev;
+    }
+
+    // 3
+    vector<string> ret;
+    string path;
+    vector<string> letterCasePermutation(string s)
+    {
+        dfs(s, 0);
+        return ret;
+    }
+
+    void dfs(string &s, int pos)
+    {
+        if (pos == s.size())
+        {
+            ret.push_back(path);
+            return;
+        }
+
+        char ch = s[pos];
+        path.push_back(ch);
+        dfs(s, pos + 1);
+        path.pop_back();
+
+        if (!isdigit(ch))
+        {
+            char tmp = change(ch);
+            path.push_back(tmp);
+            dfs(s, pos + 1);
+            path.pop_back();
+        }
+    }
+
+    char change(char ch)
+    {
+        if (ch >= 'a' && ch <= 'z')
+            ch -= 32;
+        else
+            ch += 32;
+        return ch;
+    }
+
+    // 4
+    int calculate(string s)
+    {
+        vector<int> nums;
+        char op = '+';
+
+        for (int i = 0; i < s.size();)
+        {
+            if (s[i] == ' ')
+            {
+                i++;
+                continue;
+            }
+            else
+            {
+                if (isdigit(s[i]))
+                {
+                    int tmp = 0;
+                    while (i < s.size() && isdigit(s[i]))
+                        tmp = tmp * 10 + (s[i++] - '0');
+
+                    if (op == '+')
+                        nums.push_back(tmp);
+                    else if (op == '-')
+                        nums.push_back(-tmp);
+                    else if (op == '*')
+                        nums.back() *= tmp;
+                    else
+                        nums.back() /= tmp;
+                }
+                else
+                    op = s[i++];
+            }
+        }
+
+        int ret = 0;
+        for (auto n : nums)
+            ret += n;
+        return ret;
+    }
+
+    // 5
+    int dx[4] = {0, 0, 1, -1};
+    int dy[4] = {1, -1, 0, 0};
+    int m, n;
+    vector<vector<int>> pacificAtlantic(vector<vector<int>> &heights)
+    {
+        m = heights.size(), n = heights[0].size();
+        vector<vector<bool>> pac(m, vector<bool>(n));
+        vector<vector<bool>> atl(m, vector<bool>(n));
+
+        for (int i = 0; i < m; i++)
+        {
+            dfs(heights, i, 0, pac);
+            dfs(heights, i, n - 1, atl);
+        }
+        for (int i = 0; i < n; i++)
+        {
+            dfs(heights, 0, i, pac);
+            dfs(heights, m - 1, i, atl);
+        }
+
+        vector<vector<int>> ret;
+        for (int i = 0; i < m; i++)
+            for (int j = 0; j < n; j++)
+                if (pac[i][j] && atl[i][j])
+                    ret.push_back({i, j});
+
+        return ret;
+    }
+
+    void dfs(vector<vector<int>> &heights, int i, int j, vector<vector<bool>> &vis)
+    {
+        vis[i][j] = true;
+
+        for (int k = 0; k < 4; k++)
+        {
+            int x = i + dx[k], y = j + dy[k];
+            if (x >= 0 && x < m && y >= 0 && y < n && !vis[x][y] && heights[x][y] >= heights[i][j])
+                dfs(heights, x, y, vis);
+        }
+    }
+
+    // 6
+    string convert(string s, int numRows)
+    {
+        if (numRows == 1)
+            return s;
+
+        string ret;
+        int n = s.size(), d = 2 * numRows - 2;
+
+        for (int i = 0; i < n; i += d)
+            ret += s[i];
+        for (int k = 1; k < numRows - 1; k++)
+            for (int i = k, j = d - k; i < n || j < n; i += d, j += d)
+            {
+                if (i < n)
+                    ret += s[i];
+                if (j < n)
+                    ret += s[j];
+            }
+        for (int i = numRows - 1; i < n; i += d)
+            ret += s[i];
+
+        return ret;
+    }
+};
+
+// 2
+class LRUCache
+{
+public:
+    LRUCache(int capacity)
+        : _k(capacity)
+    {
+    }
+
+    int get(int key)
+    {
+        auto ret = _hash.find(key);
+        if (ret != _hash.end())
+        {
+            LtItor it = ret->second;
+            _list.splice(_list.begin(), _list, it);
+            return it->second;
+        }
+        else
+            return -1;
+    }
+
+    void put(int key, int value)
+    {
+        auto ret = _hash.find(key);
+        if (ret != _hash.end())
+        {
+            LtItor it = ret->second;
+            _list.splice(_list.begin(), _list, it);
+            it->second = value;
+        }
+        else
+        {
+            if (_hash.size() == _k)
+            {
+                pair<int, int> back = _list.back();
+                _hash.erase(back.first);
+                _list.pop_back();
+            }
+            _list.push_front({key, value});
+            _hash[key] = _list.begin();
+        }
+    }
+
+private:
+    typedef list<pair<int, int>>::iterator LtItor;
+    int _k;
+    list<pair<int, int>> _list;
+    unordered_map<int, LtItor> _hash;
+};
