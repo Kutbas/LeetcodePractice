442b6064b8dc1be4fef7a8f0a828af63d33e76ed
Kevin <kutbass@163.com>
2025-09-22 17:37:55 +0800

    20250922

---
+++ 20250922.cc
@@ -0,0 +1,195 @@
+#include <bits/stdc++.h>
+#include <mutex>
+using namespace std;
+
+struct ListNode
+{
+    int val;
+    ListNode *next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
+
+struct TreeNode
+{
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+class Solution
+{
+public:
+    const int INF = 0x3f3f3f;
+    int maxProfit(vector<int> &prices)
+    {
+        int n = prices.size();
+        vector<vector<int>> f(n, vector<int>(3, -INF));
+        auto g = f;
+
+        f[0][0] = -prices[0], g[0][0] = 0;
+        for (int i = 1; i < n; i++)
+            for (int j = 0; j < 3; j++)
+            {
+                f[i][j] = max(f[i - 1][j], g[i - 1][j] - prices[i]);
+                g[i][j] = g[i - 1][j];
+                if (j >= 1)
+                    g[i][j] = max(g[i][j], f[i - 1][j - 1] + prices[i]);
+            }
+
+        int ret = 0;
+        for (int i = 0; i < 3; i++)
+            ret = max(ret, g[n - 1][i]);
+        
+        return ret;
+    }
+
+    // 1
+    vector<vector<int>> generate(int numRows)
+    {
+        vector<vector<int>> ret(numRows);
+        for (int i = 0; i < numRows; i++)
+        {
+            ret.resize(i + 1);
+            ret[i][0] = ret[i][i] = 1;
+            for (int j = 0; j < i + 1; j++)
+                ret[i][j] = ret[i - 1][j] + ret[i - 1][j - 1];
+        }
+
+        return ret;
+    }
+
+    // 2
+    int subarraySum(vector<int> &nums, int k)
+    {
+        unordered_map<int, int> hash;
+        hash[0] = 1;
+
+        int sum = 0, ret = 0;
+
+        for (auto n : nums)
+        {
+            sum += n;
+            if (hash.count(sum - k))
+                ret += hash[sum - k];
+            hash[sum]++;
+        }
+
+        return ret;
+    }
+
+    // 3
+    vector<int> twoSum(vector<int> &nums, int target)
+    {
+        unordered_map<int, int> hash;
+        for (int i = 0; i < nums.size(); i++)
+        {
+            int x = target - nums[i];
+            if (hash.count(x))
+                return {i, hash[x]};
+            hash[nums[i]] = i;
+        }
+
+        return {-1, -1};
+    }
+
+    // 4
+    int longestOnes(vector<int> &nums, int k)
+    {
+        int ret = 0;
+        for (int left = 0, right = 0, count = 0; right < nums.size(); right++)
+        {
+            if (nums[right] == 0)
+                count++;
+            while (count > k)
+                if (nums[left++] == 0)
+                    count--;
+            ret = max(ret, right - left + 1);
+        }
+
+        return ret;
+    }
+
+    // 5
+    vector<int> ret;
+    vector<int> tmp;
+    vector<int> index;
+    vector<int> tmpIndex;
+    vector<int> countSmaller(vector<int> &nums)
+    {
+        int n = nums.size();
+        ret.resize(n);
+        tmp.resize(n);
+        index.resize(n);
+        tmpIndex.resize(n);
+
+        for (int i = 0; i < n; i++)
+            index[i] = i;
+
+        mergeSort(nums, 0, nums.size() - 1);
+
+        return ret;
+    }
+
+    void mergeSort(vector<int> &nums, int left, int right)
+    {
+        if (left >= right)
+            return;
+
+        int mid = left + (right - left) / 2;
+        mergeSort(nums, left, mid);
+        mergeSort(nums, mid + 1, right);
+
+        int i = 0, cur1 = left, cur2 = mid + 1;
+        while (cur1 <= mid && cur2 <= right)
+        {
+            if (nums[cur1] <= nums[cur2])
+            {
+                tmp[i] = nums[cur2];
+                tmpIndex[i++] = index[cur2++];
+            }
+            else
+            {
+                ret[index[cur1]] += right - cur2 + 1;
+                tmp[i] = nums[cur1];
+                tmpIndex[i++] = index[cur1++];
+            }
+        }
+        while (cur1 <= mid)
+        {
+            tmp[i] = nums[cur1];
+            tmpIndex[i++] = index[cur1++];
+        }
+        while (cur2 <= right)
+        {
+            tmp[i] = nums[cur2];
+            tmpIndex[i++] = index[cur2++];
+        }
+
+        for (int j = left; j <= right; j++)
+        {
+            nums[j] = tmp[j - left];
+            index[j] = tmpIndex[j - left];
+        }
+    }
+
+    // 6
+    bool isPalindrome(string s)
+    {
+        string tmp;
+        for (char ch : s)
+            if (isalnum(ch))
+                tmp += tolower(ch);
+
+        int n = tmp.size();
+        for (int left = 0, right = n - 1; left < right; left++, right--)
+            if (tmp[left] != tmp[right])
+                return false;
+
+        return true;
+    }
+};
