68aa0cdf680ddd8f841e1dba4eb3bc9585e2f660
Kevin <kutbass@163.com>
2025-12-11 08:56:11 +0800

    20251211

---
+++ 20251211.cc
@@ -0,0 +1,234 @@
+#include <bits/stdc++.h>
+#include <mutex>
+using namespace std;
+
+struct ListNode
+{
+    int val;
+    ListNode *next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
+
+struct TreeNode
+{
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+class Solution
+{
+public:
+    // 1
+    int dx[4] = {0, 0, 1, -1};
+    int dy[4] = {1, -1, 0, 0};
+    int m, n;
+    vector<vector<int>> updateMatrix(vector<vector<int>> &mat)
+    {
+        m = mat.size(), n = mat[0].size();
+
+        vector<vector<int>> dist(m, vector<int>(n, -1));
+        queue<pair<int, int>> q;
+        for (int i = 0; i < m; i++)
+            for (int j = 0; j < n; j++)
+                if (mat[i][j] == 0)
+                {
+                    dist[i][j] = 0;
+                    q.push({i, j});
+                }
+
+        while (q.size())
+        {
+            int sz = q.size();
+            while (sz--)
+            {
+                auto [a, b] = q.front();
+                q.pop();
+
+                for (int k = 0; k < 4; k++)
+                {
+                    int x = a + dx[k], y = b + dy[k];
+                    if (x >= 0 && x < m && y >= 0 && y < n && dist[x][y] == -1)
+                    {
+                        dist[x][y] = dist[a][b] + 1;
+                        q.push({x, y});
+                    }
+                }
+            }
+        }
+
+        return dist;
+    }
+
+    // 2
+    int dx[4] = {0, 0, 1, -1};
+    int dy[4] = {1, -1, 0, 0};
+    int m, n;
+    bool vis[101][101];
+    int nearestExit(vector<vector<char>> &maze, vector<int> &entrance)
+    {
+        m = maze.size(), n = maze[0].size();
+
+        int bx = entrance[0], by = entrance[1];
+        queue<pair<int, int>> q;
+        q.push({bx, by});
+        vis[bx][by] = true;
+
+        int ret = 0;
+        while (q.size())
+        {
+            ret++;
+            int sz = q.size();
+            while (sz--)
+            {
+                auto [a, b] = q.front();
+                q.pop();
+                for (int k = 0; k < 4; k++)
+                {
+                    int x = a + dx[k], y = b + dy[k];
+                    if (x >= 0 && x < m && y >= 0 && y < n && !vis[x][y] && maze[x][y] == '.')
+                    {
+                        if (x == 0 || x == m - 1 || y == 0 || y == n - 1)
+                            return ret;
+                        q.push({x, y});
+                        vis[x][y] = true;
+                    }
+                }
+            }
+        }
+
+        return -1;
+    }
+
+    // 3
+    int dx[4] = {0, 0, 1, -1};
+    int dy[4] = {1, -1, 0, 0};
+    int m, n;
+    bool vis[16][16];
+    int getMaximumGold(vector<vector<int>> &grid)
+    {
+        m = grid.size(), n = grid[0].size();
+
+        int ret = 0;
+        for (int i = 0; i < m; i++)
+            for (int j = 0; j < n; j++)
+                if (grid[i][j])
+                {
+                    vis[i][j] = true;
+                    ret = max(ret, grid[i][j] + dfs(grid, i, j));
+                    vis[i][j] = false;
+                }
+
+        return ret;
+    }
+
+    int dfs(vector<vector<int>> &grid, int i, int j)
+    {
+        int ret = 0;
+
+        for (int k = 0; k < 4; k++)
+        {
+            int x = i + dx[k], y = j + dy[k];
+            if (x >= 0 && x < m && y >= 0 && y < n && !vis[x][y] && grid[x][y])
+            {
+                vis[x][y] = true;
+                ret = max(ret, grid[x][y] + dfs(grid, x, y));
+                vis[x][y] = false;
+            }
+        }
+
+        return ret;
+    }
+
+    // 4
+    int brokenCalc(int startValue, int target)
+    {
+        int ret = 0;
+        while (target > startValue)
+        {
+            if (target % 2 == 0)
+                target /= 2;
+            else
+                target++;
+            ret++;
+        }
+        return ret + startValue - target;
+    }
+
+    // 5
+    bool isInterleave(string s1, string s2, string s3)
+    {
+        int m = s1.size(), n = s2.size();
+        if (m + n != s3.size())
+            return false;
+
+        s1 = " " + s1;
+        s2 = " " + s2;
+        s3 = " " + s3;
+
+        vector<vector<bool>> dp(m + 1, vector<bool>(n + 1));
+        dp[0][0] = true;
+
+        for (int i = 1; i <= m; i++)
+            if (s1[i] == s3[i])
+                dp[i][0] = dp[i - 1][0];
+            else
+                break;
+
+        for (int i = 1; i <= n; i++)
+            if (s2[i] == s3[i])
+                dp[0][i] = dp[0][i - 1];
+            else
+                break;
+
+        for (int i = 1; i <= m; i++)
+            for (int j = 1; j <= n; j++)
+            {
+                bool from_s1 = s1[i] == s3[i + j] && dp[i - 1][j];
+                bool from_s2 = s2[j] == s3[i + j] && dp[i][j - 1];
+
+                dp[i][j] = from_s1 || from_s2;
+            }
+
+        return dp[m][n];
+    }
+
+    // 6
+    bool increasingTriplet(vector<int> &nums)
+    {
+        int a = INT_MAX, b = INT_MAX;
+        for (int i = 0; i < nums.size(); i++)
+        {
+            if (nums[i] > a)
+                return true;
+            else if (nums[i] > b)
+                a = nums[i];
+            else
+                b = nums[i];
+        }
+        return false;
+    }
+
+    // 7
+    int deleteAndEarn(vector<int> &nums)
+    {
+        const int N = 10001;
+        int arr[N] = {0};
+        for (auto n : nums)
+            arr[n] += n;
+
+        vector<int> f(N), g(N);
+        for (int i = 1; i < N; i++)
+        {
+            f[i] = f[i - 1] + arr[i];
+            g[i] = max(f[i - 1], g[i - 1]);
+        }
+
+        return max(f[N - 1], g[N - 1]);
+    }
+};
