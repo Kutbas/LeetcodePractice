f6acb43a9df539cdc9767015976b80eafbf29fde
ROG STRIX <1820533976@qq.com>
2026-01-01 14:04:24 +0800

    20260101

---
+++ 20260101.cc
@@ -0,0 +1,262 @@
+#include <bits/stdc++.h>
+#include <mutex>
+using namespace std;
+
+struct ListNode
+{
+    int val;
+    ListNode *next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
+
+struct TreeNode
+{
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+class Solution
+{
+public:
+    // 1
+    vector<int> missingTwo(vector<int> &nums)
+    {
+        int xorsum = 0;
+        for (auto n : nums)
+            xorsum ^= n;
+        for (int i = 1; i <= nums.size() + 2; i++)
+            xorsum ^= i;
+
+        int lsb = xorsum == INT_MIN ? xorsum : xorsum & (-xorsum);
+        int x = 0, y = 0;
+        for (auto n : nums)
+        {
+            if (n & lsb)
+                x ^= n;
+            else
+                y ^= n;
+        }
+        for (int i = 1; i <= nums.size() + 2; i++)
+        {
+            if (i & lsb)
+                x ^= i;
+            else
+                y ^= i;
+        }
+
+        return {x, y};
+    }
+
+    // 2
+    bool isInterleave(string s1, string s2, string s3)
+    {
+        int m = s1.size(), n = s2.size();
+        if (m + n != s3.size())
+            return false;
+
+        s1 = " " + s1;
+        s2 = " " + s2;
+        s3 = " " + s3;
+
+        vector<vector<bool>> dp(m + 1, vector<bool>(n + 1));
+
+        for (int i = 1; i <= m; i++)
+            if (s1[i] == s3[i])
+                dp[i][0] = true;
+            else
+                break;
+
+        for (int i = 1; i <= n; i++)
+            if (s2[i] == s3[i])
+                dp[0][i] = true;
+            else
+                break;
+
+        for (int i = 1; i <= m; i++)
+            for (int j = 1; j <= n; j++)
+            {
+                bool from_s1 = s1[i] == s3[i + j] && dp[i - 1][j];
+                bool from_s2 = s2[j] == s3[i + j] && dp[i][j - 1];
+
+                dp[i][j] = from_s1 || from_s2;
+            }
+
+        return dp[m][n];
+    }
+
+    // 3
+    int N, left = 0, right = 0;
+    vector<string> ret;
+    string path;
+    vector<string> generateParenthesis(int n)
+    {
+        N = n;
+        dfs();
+        return ret;
+    }
+
+    void dfs()
+    {
+        if (right == N)
+        {
+            ret.push_back(path);
+            return;
+        }
+
+        if (left < N)
+        {
+            path.push_back('(');
+            left++;
+            dfs();
+            left--;
+            path.pop_back();
+        }
+
+        if (right < left)
+        {
+            path.push_back(')');
+            right++;
+            dfs();
+            right--;
+            path.pop_back();
+        }
+    }
+
+    // 4
+    int m, n;
+    int dx[4] = {0, 0, 1, -1};
+    int dy[4] = {1, -1, 0, 0};
+    bool vis[501][501] = {0};
+    int numEnclaves(vector<vector<int>> &grid)
+    {
+        m = grid.size(), n = grid[0].size();
+
+        for (int i = 0; i < m; i++)
+        {
+            if (grid[i][0] == 1)
+                dfs(grid, i, 0);
+            if (grid[i][n - 1] == 1)
+                dfs(grid, i, n - 1);
+        }
+
+        for (int i = 0; i < n; i++)
+        {
+            if (grid[0][i] == 1)
+                dfs(grid, 0, i);
+            if (grid[m - 1][i] == 1)
+                dfs(grid, m - 1, i);
+        }
+
+        int ret = 0;
+        for (int i = 0; i < m; i++)
+            for (int j = 0; j < n; j++)
+                if (grid[i][j] == 1 && !vis[i][j])
+                    ret++;
+        return ret;
+    }
+
+    void dfs(vector<vector<int>> &grid, int i, int j)
+    {
+        queue<pair<int, int>> q;
+        q.push({i, j});
+        vis[i][j] = true;
+
+        while (q.size())
+        {
+            auto [a, b] = q.front();
+            q.pop();
+            for (int k = 0; k < 4; k++)
+            {
+                int x = a + dx[k], y = b + dy[k];
+                if (x >= 0 && x < m && y >= 0 && y < n && !vis[x][y] && grid[x][y] == 1)
+                {
+                    q.push({x, y});
+                    vis[x][y] = true;
+                }
+            }
+        }
+    }
+
+    // 5
+    int tmp[50010];
+    int reversePairs(vector<int> &record)
+    {
+        return mergeSort(record, 0, record.size() - 1);
+    }
+
+    int mergeSort(vector<int> &nums, int left, int right)
+    {
+        if (left >= right)
+            return 0;
+
+        int mid = left + (right - left) / 2;
+        int ret = 0;
+        ret += mergeSort(nums, left, mid);
+        ret += mergeSort(nums, mid + 1, right);
+
+        int i = 0, cur1 = left, cur2 = mid + 1;
+        while (cur1 <= mid && cur2 <= right)
+        {
+            if (nums[cur1] <= nums[cur2])
+                tmp[i++] = nums[cur2++];
+            else
+            {
+                ret += right - cur2 + 1;
+                tmp[i++] = nums[cur1++];
+            }
+        }
+        while (cur1 <= mid)
+            tmp[i++] = nums[cur1++];
+        while (cur2 <= right)
+            tmp[i++] = nums[cur2++];
+
+        for (int j = left; j <= right; j++)
+            nums[j] = tmp[j - left];
+
+        return ret;
+    }
+
+    // 6
+    void hanota(vector<int> &A, vector<int> &B, vector<int> &C)
+    {
+        dfs(A, B, C, A.size());
+    }
+
+    void dfs(vector<int> &from, vector<int> &buffer, vector<int> &to, int n)
+    {
+        if (n == 1)
+        {
+            to.push_back(from.back());
+            from.pop_back();
+            return;
+        }
+
+        dfs(from, to, buffer, n - 1);
+        to.push_back(from.back());
+        from.pop_back();
+        dfs(buffer, from, to, n - 1);
+    }
+
+    // 7
+    int brokenCalc(int startValue, int target)
+    {
+        int ret = 0;
+        while (target > startValue)
+        {
+            if (target % 2 == 0)
+                target /= 2;
+            else
+                target++;
+
+            ret++;
+        }
+
+        return ret + startValue - target;
+    }
+};
