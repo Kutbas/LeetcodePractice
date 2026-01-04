c40f3b4b63ca7d3762dea826cc60bf9dac64890c
Kevin <kutbass@163.com>
2026-01-04 08:25:55 +0800

    20260104

---
+++ 20260104.cc
@@ -0,0 +1,82 @@
+#include <bits/stdc++.h>
+#include <mutex>
+using namespace std;
+
+struct ListNode
+{
+    int val;
+    ListNode *next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
+
+struct TreeNode
+{
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+class Solution
+{
+public:
+    // 1
+    int deleteAndEarn(vector<int> &nums)
+    {
+        const int N = 10001;
+        int arr[N] = {0};
+
+        for (auto n : nums)
+            arr[n] += n;
+
+        vector<int> f(N), g(N);
+
+        for (int i = 1; i < N; i++)
+        {
+            f[i] = g[i - 1] + arr[i];
+            g[i] = max(f[i - 1], g[i - 1]);
+        }
+
+        return max(f[N - 1], g[N - 1]);
+    }
+
+    // 2
+    int calculateMinimumHP(vector<vector<int>> &dungeon)
+    {
+        int m = dungeon.size(), n = dungeon[0].size();
+
+        vector<vector<int>> dp(m + 1, vector<int>(n + 1, INT_MAX));
+        dp[m][n - 1] = dp[m - 1][n] = 1;
+
+        for (int i = m - 1; i >= 0; i--)
+            for (int j = n - 1; j >= 0; j--)
+            {
+                int remain = min(dp[i + 1][j], dp[i][j + 1]);
+
+                dp[i][j] = remain - dungeon[i][j];
+                dp[i][j] = max(1, dp[i][j]);
+            }
+
+        return dp[0][0];
+    }
+
+    // 3
+    int removeDuplicates(vector<int> &nums)
+    {
+        int n = nums.size(), slow = 1, fast = 1;
+
+        while (fast < n)
+        {
+            if (nums[fast] != nums[fast - 1])
+                nums[slow++] = nums[fast];
+
+            fast++;
+        }
+
+        return slow;
+    }
+};
