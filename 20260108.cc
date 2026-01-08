de7487a5cbac13a7bf3055b0a65a49ff5fe8a17d
ROG STRIX <1820533976@qq.com>
2026-01-08 11:54:39 +0800

    20260108

---
+++ 20260108.cc
@@ -0,0 +1,130 @@
+#include <bits/stdc++.h>
+#include <mutex>
+using namespace std;
+
+struct ListNode
+{
+    int val;
+    ListNode *next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
+
+struct TreeNode
+{
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+class Solution
+{
+public:
+    // 1
+    int maxEnvelopes(vector<vector<int>> &e)
+    {
+        int n = e.size();
+        sort(e.begin(), e.end(), [&](const vector<int> &v1, const vector<int> &v2)
+             { return v1[0] != v2[0] ? v1[0] < v2[0] : v1[1] > v2[1]; });
+
+        vector<int> ret;
+        ret.push_back(e[0][1]);
+        for (int i = 1; i < n; i++)
+        {
+            if (e[i][1] > ret.back())
+                ret.push_back(e[i][1]);
+            else
+            {
+                int left = 0, right = ret.size() - 1;
+                while (left < right)
+                {
+                    int mid = left + (right - left) / 2;
+                    if (e[i][1] > ret[mid])
+                        left = mid + 1;
+                    else
+                        right = mid;
+                }
+                ret[left] = e[i][1];
+            }
+        }
+
+        return ret.size();
+    }
+
+    // 2
+    vector<int> advantageCount(vector<int> &nums1, vector<int> &nums2)
+    {
+        int n = nums1.size();
+        vector<int> index2(n);
+        for (int i = 0; i < n; i++)
+            index2[i] = i;
+
+        sort(nums1.begin(), nums1.end());
+        sort(index2.begin(), index2.end(), [&](int i, int j)
+             { return nums2[i] < nums2[j]; });
+
+        vector<int> ret(n);
+        int left = 0, right = n - 1;
+        for (auto n : nums1)
+        {
+            if (n > nums2[index2[left]])
+                ret[index2[left++]] = n;
+            else
+                ret[index2[right--]] = n;
+        }
+
+        return ret;
+    }
+
+    // 3
+    int profitableSchemes(int n, int m, vector<int> &group, vector<int> &profit)
+    {
+        const int MOD = 1e9 + 7;
+        int len = group.size();
+        vector<vector<vector<int>>> dp(len + 1, vector<vector<int>>(n + 1, vector<int>(m + 1)));
+
+        for (int i = 0; i <= n; i++)
+            dp[0][i][0] = 1;
+
+        for (int i = 1; i <= len; i++)
+            for (int j = 0; j <= n; j++)
+                for (int k = 0; k <= m; k++)
+                {
+                    dp[i][j][k] = dp[i - 1][j][k];
+
+                    if (j >= group[i - 1])
+                    {
+                        int remain = max(0, k - profit[i - 1]);
+
+                        dp[i][j][k] += dp[i - 1][j - group[i - 1]][remain];
+                    }
+
+                    dp[i][j][k] %= MOD;
+                }
+
+        return dp[len][n][m];
+    }
+
+    // 4
+    int findMaxLength(vector<int> &nums)
+    {
+        int ret = 0, sum = 0;
+        unordered_map<int, int> hash;
+        hash[0] = -1;
+
+        for (int i = 0; i < nums.size(); i++)
+        {
+            sum += nums[i] == 0 ? -1 : 1;
+            if (hash.count(sum))
+                ret = max(ret, i - hash[sum]);
+            else
+                hash[sum] = i;
+        }
+
+        return ret;
+    }
+};
