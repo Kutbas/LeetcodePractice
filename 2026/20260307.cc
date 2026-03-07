b6df1cc715dc4ae1c54b31c61207ad7b92fd455b
Kevin <kutbas@KevindeMacBook-Pro.local>
2026-03-07 18:54:39 +0800

    20260307

---
+++ 2026/20260307.cc
@@ -0,0 +1,256 @@
+#include <bits/stdc++.h>
+#include <mutex>
+using namespace std;
+
+struct ListNode
+{
+    int val;
+    ListNode *next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
+
+struct TreeNode
+{
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+class Solution
+{
+public:
+    // 1
+    int numSquares(int n)
+    {
+        int m = sqrt(n);
+        vector<vector<int>> dp(m + 1, vector<int>(n + 1));
+        for (int i = 1; i <= n; i++)
+            dp[0][i] = INT_MAX;
+
+        for (int i = 1; i <= m; i++)
+            for (int j = 0; j <= n; j++)
+            {
+                dp[i][j] = dp[i - 1][j];
+                if (j >= i * i)
+                    dp[i][j] = min(dp[i][j], dp[i][j - i * i] + 1);
+            }
+
+        return dp[m][n];
+    }
+
+    // 2
+    int combinationSum4(vector<int> &nums, int target)
+    {
+        vector<double> dp(target + 1);
+        dp[0] = 1;
+
+        for (int i = 1; i <= target; i++)
+            for (auto n : nums)
+            {
+                if (i >= n)
+                    dp[i] += dp[i - n];
+            }
+
+        return dp[target];
+    }
+
+    // 3
+    bool isInterleave(string s1, string s2, string s3)
+    {
+        int m = s1.size(), n = s2.size();
+        if (m + n != s3.size())
+            return false;
+
+        vector<vector<bool>> dp(m + 1, vector<bool>(n + 1));
+        dp[0][0] = true;
+
+        s1 = " " + s1;
+        s2 = " " + s2;
+        s3 = " " + s3;
+
+        for (int i = 1; i <= m; i++)
+            if (s1[i] == s3[i])
+                dp[i][0] = true;
+            else
+                break;
+
+        for (int i = 1; i <= n; i++)
+            if (s2[i] == s3[i])
+                dp[0][i] = true;
+            else
+                break;
+
+        for (int i = 1; i <= m; i++)
+            for (int j = 1; j <= n; j++)
+            {
+                bool from_s1 = s1[i] == s3[i + j] && dp[i - 1][j];
+                bool from_s2 = s2[j] == s3[i + j] && dp[i][j - 1];
+
+                dp[i][j] = from_s1 || from_s2;
+            }
+
+        return dp[m][n];
+    }
+
+    // 4
+    int massage(vector<int> &nums)
+    {
+        int n = nums.size();
+        if (n == 0)
+            return 0;
+        vector<int> f(n), g(n);
+        f[0] = nums[0];
+
+        for (int i = 1; i < n; i++)
+        {
+            f[i] = g[i - 1] + nums[i];
+            g[i] = max(f[i - 1], g[i - 1]);
+        }
+
+        return max(f[n - 1], g[n - 1]);
+    }
+
+    // 5
+    int tmp[50010];
+    vector<int> sortArray(vector<int> &nums)
+    {
+        mergeSort(nums, 0, nums.size() - 1);
+        return nums;
+    }
+
+    void mergeSort(vector<int> &nums, int left, int right)
+    {
+        if (left >= right)
+            return;
+
+        int mid = left + (right - left) / 2;
+        mergeSort(nums, left, mid);
+        mergeSort(nums, mid + 1, right);
+
+        int i = 0, cur1 = left, cur2 = mid + 1;
+        while (cur1 <= mid && cur2 <= right)
+            tmp[i++] = nums[cur1] <= nums[cur2] ? nums[cur1++] : nums[cur2++];
+        while (cur1 <= mid)
+            tmp[i++] = nums[cur1++];
+        while (cur2 <= right)
+            tmp[i++] = nums[cur2++];
+
+        for (int j = left; j <= right; j++)
+            nums[j] = tmp[j - left];
+    }
+
+    // 6
+    int m, n;
+    int dx[8] = {0, 0, 1, -1, 1, 1, -1, -1};
+    int dy[8] = {1, -1, 0, 0, 1, -1, 1, -1};
+    vector<vector<char>> updateBoard(vector<vector<char>> &board, vector<int> &click)
+    {
+        m = board.size(), n = board[0].size();
+        int bx = click[0], by = click[1];
+
+        if (board[bx][by] == 'M')
+        {
+            board[bx][by] = 'X';
+            return board;
+        }
+        else
+            dfs(board, bx, by);
+
+        return board;
+    }
+
+    void dfs(vector<vector<char>> &board, int i, int j)
+    {
+        int count = 0;
+
+        for (int k = 0; k < 8; k++)
+        {
+            int x = i + dx[k], y = j + dy[k];
+            if (x >= 0 && x < m && y >= 0 && y < n && board[x][y] == 'M')
+                count++;
+        }
+
+        if (count)
+            board[i][j] = count + '0';
+        else
+        {
+            board[i][j] = 'B';
+            for (int k = 0; k < 8; k++)
+            {
+                int x = i + dx[k], y = j + dy[k];
+                if (x >= 0 && x < m && y >= 0 && y < n && board[x][y] == 'E')
+                    dfs(board, x, y);
+            }
+        }
+    }
+
+    // 7
+    int eraseOverlapIntervals(vector<vector<int>> &in)
+    {
+        sort(in.begin(), in.end());
+        int n = in.size(), right = in[0][1], ret = 0;
+
+        for (int i = 1; i < n; i++)
+        {
+            int a = in[i][0], b = in[i][1];
+
+            if (a < right)
+            {
+                ret++;
+                right = min(right, b);
+            }
+            else
+                right = b;
+        }
+
+        return ret;
+    }
+
+    // 8
+    string longestCommonPrefix(vector<string> &strs)
+    {
+        for (int i = 0; i < strs[0].size(); i++)
+            for (int j = 1; j < strs.size(); j++)
+                if (i == strs[j].size() || strs[0][i] != strs[j][i])
+                    return strs[0].substr(0, i);
+        return strs[0];
+    }
+
+    // 9
+    bool isMatch(string s, string p)
+    {
+        int m = s.size(), n = p.size();
+        vector<vector<bool>> dp(m + 1, vector<bool>(n + 1));
+
+        s = " " + s;
+        p = " " + p;
+
+        dp[0][0] = true;
+
+        for (int i = 2; i <= n; i += 2)
+            if (p[i] == '*')
+                dp[0][i] = true;
+            else
+                break;
+
+        for (int i = 1; i <= m; i++)
+            for (int j = 1; j <= n; j++)
+            {
+                if (p[j] == '*')
+                {
+                    bool case1 = dp[i][j - 2];
+                    bool case2 = (p[j - 1] == '.' || p[j - 1] == s[i]) && dp[i - 1][j];
+                    dp[i][j] = case1 || case2;
+                }
+                else
+                    dp[i][j] = (p[j] == s[i] || p[j] == '.') && dp[i - 1][j - 1];
+            }
+
+        return dp[m][n];
+    }
+};
