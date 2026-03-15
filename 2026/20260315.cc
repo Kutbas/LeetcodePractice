acee1db9a59f44b87f0d0e3ffc2fc0a09e107ae5
Kevin's Mac <kutbass@163.com>
2026-03-15 19:22:24 +0800

    20260315

---
+++ 2026/20260315.cc
@@ -0,0 +1,256 @@
+#include <bits/stdc++.h>
+#include <mutex>
+using namespace std;
+
+struct ListNode
+{
+    int val;
+    ListNode *next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
+
+struct TreeNode
+{
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+class Solution
+{
+public:
+    // 1
+    int massage(vector<int> &nums)
+    {
+        int n = nums.size();
+        if (n == 0)
+            return 0;
+        vector<int> f(n), g(n);
+        f[0] = nums[0];
+
+        for (int i = 1; i < n; i++)
+        {
+            f[i] = g[i - 1] + nums[i];
+            g[i] = max(f[i - 1], g[i - 1]);
+        }
+
+        return max(f[n - 1], g[n - 1]);
+    }
+
+    // 2
+    int lastStoneWeightII(vector<int> &stones)
+    {
+        int n = stones.size(), sum = 0;
+        for (auto s : stones)
+            sum += s;
+
+        int m = sum / 2;
+        vector<vector<int>> dp(n + 1, vector<int>(m + 1));
+        for (int i = 1; i <= n; i++)
+            for (int j = 0; j <= m; j++)
+            {
+                dp[i][j] = dp[i - 1][j];
+
+                if (j >= stones[i - 1])
+                    dp[i][j] = max(dp[i][j], dp[i - 1][j - stones[i - 1]] + stones[i - 1]);
+            }
+
+        return sum - 2 * dp[n][m];
+    }
+
+    // 3
+    int findLength(vector<int> &nums1, vector<int> &nums2)
+    {
+        int m = nums1.size(), n = nums2.size(), ret = 0;
+        vector<vector<int>> dp(m + 1, vector<int>(n + 1));
+
+        for (int i = 1; i <= m; i++)
+            for (int j = 1; j <= n; j++)
+            {
+                if (nums1[i - 1] == nums2[j - 1])
+                    dp[i][j] = dp[i - 1][j - 1] + 1;
+                ret = max(ret, dp[i][j]);
+            }
+
+        return ret;
+    }
+
+    // 4
+    vector<int> findOrder(int numCourses, vector<vector<int>> &prerequisites)
+    {
+        vector<int> in(numCourses);
+        vector<vector<int>> edges(numCourses);
+
+        for (auto &e : prerequisites)
+        {
+            int a = e[0], b = e[1]; // b->a;
+            in[a]++;
+            edges[b].push_back(a);
+        }
+
+        vector<int> ret;
+        queue<int> q;
+        for (int i = 0; i < numCourses; i++)
+            if (in[i] == 0)
+                q.push(i);
+        while (q.size())
+        {
+            auto t = q.front();
+            q.pop();
+            ret.push_back(t);
+            for (auto a : edges[t])
+                if (--in[a] == 0)
+                    q.push(a);
+        }
+
+        for (int i = 0; i < numCourses; i++)
+            if (in[i])
+                return vector<int>{};
+
+        return ret;
+    }
+
+    // 6
+    int m, n;
+    int dx[4] = {0, 0, 1, -1};
+    int dy[4] = {1, -1, 0, 0};
+    int calculateMinimumHP(vector<vector<int>> &dungeon)
+    {
+        m = dungeon.size(), n = dungeon[0].size();
+        vector<vector<int>> dp(m + 1, vector<int>(n + 1, INT_MAX));
+        dp[m][n - 1] = dp[m - 1][n] = 1;
+
+        for (int i = m - 1; i >= 0; i--)
+            for (int j = n - 1; j >= 0; j--)
+            {
+                int prev = min(dp[i + 1][j], dp[i][j + 1]);
+                dp[i][j] = prev - dungeon[i][j];
+                dp[i][j] = max(1, dp[i][j]);
+            }
+
+        return dp[0][0];
+    }
+
+    // 7
+    int tmp[50010];
+    int reversePairs(vector<int> &nums)
+    {
+        return mergeSort(nums, 0, nums.size() - 1);
+    }
+
+    int mergeSort(vector<int> &nums, int left, int right)
+    {
+        if (left >= right)
+            return 0;
+
+        int mid = left + (right - left) / 2, ret = 0;
+        ret += mergeSort(nums, left, mid);
+        ret += mergeSort(nums, mid + 1, right);
+
+        int i = 0, cur1 = left, cur2 = mid + 1;
+        while (cur1 <= mid && cur2 <= right)
+        {
+            if (nums[cur1] / 2.0 <= nums[cur2])
+                cur2++;
+            else
+            {
+                ret += right - cur2 + 1;
+                cur1++;
+            }
+        }
+        cur1 = left, cur2 = mid + 1;
+        while (cur1 <= mid && cur2 <= right)
+            tmp[i++] = nums[cur1] <= nums[cur2] ? nums[cur2++] : nums[cur1++];
+        while (cur1 <= mid)
+            tmp[i++] = nums[cur1++];
+        while (cur2 <= right)
+            tmp[i++] = nums[cur2++];
+
+        for (int j = left; j <= right; j++)
+            nums[j] = tmp[j - left];
+
+        return ret;
+    }
+};
+
+// 5
+class DiningPhilosophers
+{
+public:
+    DiningPhilosophers()
+    {
+    }
+
+    void wantsToEat(int philosopher,
+                    function<void()> pickLeftFork,
+                    function<void()> pickRightFork,
+                    function<void()> eat,
+                    function<void()> putLeftFork,
+                    function<void()> putRightFork)
+    {
+        int l = philosopher, r = (philosopher + 1) % 5;
+        if (l % 2 == 0)
+        {
+            lock[l].lock();
+            lock[r].lock();
+        }
+        else
+        {
+            lock[r].lock();
+            lock[l].lock();
+        }
+        pickLeftFork();
+        pickRightFork();
+        eat();
+        putLeftFork();
+        putRightFork();
+        lock[l].unlock();
+        lock[r].unlock();
+    }
+
+private:
+    mutex lock[5];
+};
+
+// 8const int N=1010;
+int n, v;
+int V[N], W[N];
+int dp[N][N];
+
+int main()
+{
+    cin >> n >> v;
+    for (int i = 1; i <= n; i++)
+        cin >> V[i] >> W[i];
+
+    memset(dp, 0, sizeof dp);
+    for (int i = 1; i <= n; i++)
+        for (int j = 0; j <= v; j++)
+        {
+            dp[i][j] = dp[i - 1][j];
+
+            if (j >= V[i])
+                dp[i][j] = max(dp[i][j], dp[i - 1][j - V[i]] + W[i]);
+        }
+    cout << dp[n][v] << endl;
+
+    memset(dp, 0, sizeof dp);
+    for (int i = 1; i <= v; i++)
+        dp[0][i] = -1;
+    for (int i = 1; i <= n; i++)
+        for (int j = 0; j <= v; j++)
+        {
+            dp[i][j] = dp[i - 1][j];
+
+            if (j >= V[i] && dp[i - 1][j - V[i]] != -1)
+                dp[i][j] = max(dp[i][j], dp[i - 1][j - V[i]] + W[i]);
+        }
+    cout << (dp[n][v] == -1 ? 0 : dp[n][v]) << endl;
+
+    return 0;
+}
