2a84176fed8ab4f28a95b72c51d49b4d8aa3e39b
Kevin's Mac <kutbass@163.com>
2026-03-30 13:44:55 +0800

    20260330

---
+++ 2026/20260330.cc
@@ -0,0 +1,91 @@
+#include <bits/stdc++.h>
+#include <mutex>
+using namespace std;
+
+struct ListNode
+{
+    int val;
+    ListNode *next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
+
+struct TreeNode
+{
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+class Solution
+{
+public:
+    // 1
+    int combinationSum4(vector<int> &nums, int target)
+    {
+        vector<double> dp(target + 1);
+        dp[0] = 1;
+
+        for (int i = 1; i <= target; i++)
+            for (auto n : nums)
+            {
+                if (i >= n)
+                    dp[i] += dp[i - n];
+            }
+
+        return dp[target];
+    }
+
+    // 2
+    ListNode *addTwoNumbers(ListNode *l1, ListNode *l2)
+    {
+        ListNode *ret = new ListNode(0);
+        ListNode *prev = ret;
+        ListNode *cur1 = l1, *cur2 = l2;
+        int t = 0;
+
+        while (t || cur1 || cur2)
+        {
+            if (cur1)
+            {
+                t += cur1->val;
+                cur1 = cur1->next;
+            }
+            if (cur2)
+            {
+                t += cur2->val;
+                cur2 = cur2->next;
+            }
+
+            prev->next = new ListNode(t % 10);
+            prev = prev->next;
+            t /= 10;
+        }
+
+        prev = ret->next;
+        delete ret, cur1, cur2;
+        return prev;
+    }
+
+    // 3
+    vector<string> sortPeople(vector<string> &names, vector<int> &heights)
+    {
+        int n = names.size();
+        vector<int> index(n);
+        for (int i = 0; i < n; i++)
+            index[i] = i;
+
+        sort(index.begin(), index.end(), [&](const int i, const int j)
+             { return heights[i] > heights[j]; });
+
+        vector<string> ret;
+        for (auto i : index)
+            ret.push_back(names[i]);
+
+        return ret;
+    }
+};
