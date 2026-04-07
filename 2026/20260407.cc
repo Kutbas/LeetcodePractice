3f1a7ab650e5f8094b3c3c0bc5a5f7366b817fe6
Kevin's Mac <kutbass@163.com>
2026-04-07 21:56:50 +0800

    20260407

---
+++ 2026/20260407.cc
@@ -0,0 +1,209 @@
+#include <bits/stdc++.h>
+#include <mutex>
+using namespace std;
+
+struct ListNode
+{
+    int val;
+    ListNode *next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
+
+struct TreeNode
+{
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+class Solution
+{
+public:
+    // 1
+    int minCut(string s)
+    {
+        int n = s.size();
+        vector<vector<bool>> isPal(n, vector<bool>(n));
+        for (int i = n - 1; i >= 0; i--)
+            for (int j = i; j < n; j++)
+                if (s[i] == s[j])
+                    isPal[i][j] = j - i + 1 > 3 ? isPal[i + 1][j - 1] : true;
+
+        vector<int> dp(n, INT_MAX);
+        for (int i = 0; i < n; i++)
+        {
+            if (isPal[0][i])
+                dp[i] = 0;
+            else
+            {
+                for (int j = 1; j <= i; j++)
+                {
+                    if (isPal[j][i])
+                        dp[i] = min(dp[i], dp[j - 1] + 1);
+                }
+            }
+        }
+
+        return dp[n - 1];
+    }
+
+    // 2
+    vector<int> ret;
+    vector<int> preorderTraversal(TreeNode *root)
+    {
+        if (root == nullptr)
+            return ret;
+        dfs(root);
+        return ret;
+    }
+
+    void dfs(TreeNode *root)
+    {
+        if (root == nullptr)
+            return;
+
+        ret.push_back(root->val);
+        dfs(root->left);
+        dfs(root->right);
+    }
+
+    // 3
+    string longestPalindrome(string s)
+    {
+        int n = s.size(), begin = -1, length = 0;
+        vector<vector<bool>> isPal(n, vector<bool>(n));
+        for (int i = n - 1; i >= 0; i--)
+            for (int j = i; j < n; j++)
+                if (s[i] == s[j])
+                    isPal[i][j] = j - i + 1 > 3 ? isPal[i + 1][j - 1] : true;
+
+        for (int i = 0; i < n; i++)
+            for (int j = 0; j < n; j++)
+            {
+                if (isPal[i][j] && j - i + 1 > length)
+                {
+                    begin = i;
+                    length = j - i + 1;
+                }
+            }
+
+        return s.substr(begin, length);
+    }
+
+    // 4
+    ListNode *reverseList(ListNode *head)
+    {
+        if (head == nullptr || head->next == nullptr)
+            return head;
+
+        ListNode *newhead = reverseList(head->next);
+        head->next->next = head;
+        head->next = nullptr;
+
+        return newhead;
+    }
+
+    // 5
+    bool containsNearbyDuplicate(vector<int> &nums, int k)
+    {
+        unordered_map<int, int> hash;
+        for (int i = 0; i < nums.size(); i++)
+        {
+            if (hash.count(nums[i]) && i - hash[nums[i]] <= k)
+                return true;
+            hash[nums[i]] = i;
+        }
+        return false;
+    }
+
+    // 6
+    int canCompleteCircuit(vector<int> &gas, vector<int> &cost)
+    {
+        int n = gas.size();
+        for (int i = 0; i < n; i++)
+        {
+            int step = 0, rest = 0;
+            for (; step < n; step++)
+            {
+                int index = (i + step) % n;
+                rest += gas[index] - cost[index];
+                if (rest < 0)
+                    break;
+            }
+
+            if (rest >= 0)
+                return i;
+            i = i + step;
+        }
+        return -1;
+    }
+
+    // 8
+    int maxSubArray(vector<int> &nums)
+    {
+        int n = nums.size(), ret = INT_MIN;
+        vector<int> dp(n + 1);
+
+        for (int i = 1; i <= n; i++)
+        {
+            dp[i] = max(nums[i - 1], dp[i - 1] + nums[i - 1]);
+            ret = max(ret, dp[i]);
+        }
+
+        return ret;
+    }
+};
+
+// 7
+class LRUCache
+{
+public:
+    LRUCache(int capacity) : _capacity(capacity)
+    {
+    }
+
+    int get(int key)
+    {
+        auto ret = _hash.find(key);
+        if (ret != _hash.end())
+        {
+            auto it = ret->second;
+            _list.splice(_list.begin(), _list, it);
+            return it->second;
+        }
+        return -1;
+    }
+
+    void put(int key, int value)
+    {
+        auto ret = _hash.find(key);
+        if (ret != _hash.end())
+        {
+            auto it = ret->second;
+            it->second = value;
+            _list.splice(_list.begin(), _list, it);
+        }
+        else
+        {
+            if (_capacity == _hash.size())
+            {
+                pair<int, int> back = _list.back();
+                _list.pop_back();
+                _hash.erase(back.first);
+            }
+            _list.push_front({key, value});
+            _hash[key] = _list.begin();
+        }
+    }
+
+private:
+    int _capacity;
+    typedef list<pair<int, int>>::iterator LstItor;
+    list<pair<int, int>> _list;
+    unordered_map<int, LstItor> _hash;
+};
