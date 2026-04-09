003ae5855f9ab8ab02f2a5a81773696e3a29653a
Kevin's Mac <kutbass@163.com>
2026-04-09 09:08:45 +0800

    20260409

---
+++ 2026/20260409.cc
@@ -0,0 +1,140 @@
+#include <bits/stdc++.h>
+#include <mutex>
+using namespace std;
+
+struct ListNode
+{
+    int val;
+    ListNode *next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
+
+struct TreeNode
+{
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+class Solution
+{
+public:
+    // 1
+    int maxSubArray(vector<int> &nums)
+    {
+        int n = nums.size(), ret = INT_MIN;
+        vector<int> dp(n + 1);
+
+        for (int i = 1; i <= n; i++)
+        {
+            dp[i] = max(nums[i - 1], dp[i - 1] + nums[i - 1]);
+            ret = max(ret, dp[i]);
+        }
+
+        return ret;
+    }
+
+    // 2
+    string optimalDivision(vector<int> &nums)
+    {
+        int n = nums.size();
+        if (n == 1)
+            return to_string(nums[0]);
+
+        if (n == 2)
+            return to_string(nums[0]) + "/" + to_string(nums[1]);
+
+        string ret = to_string(nums[0]) + "/(" + to_string(nums[1]);
+        for (int i = 2; i < n; i++)
+            ret += "/" + to_string(nums[i]);
+
+        ret += ")";
+
+        return ret;
+    }
+
+    // 3
+    vector<string> sortPeople(vector<string> &names, vector<int> &heights)
+    {
+        int n = names.size();
+        vector<int> index(n);
+        for (int i = 0; i < n; i++)
+            index[i] = i;
+
+        sort(index.begin(), index.end(), [&](const int i, const int j)
+             { return heights[i] > heights[j]; });
+
+        vector<string> ret;
+        for (auto i : index)
+            ret.push_back(names[i]);
+
+        return ret;
+    }
+
+    // 4
+    int m, n;
+    int dx[4] = {0, 0, 1, -1};
+    int dy[4] = {1, -1, 0, 0};
+    bool vis[16][16] = {0};
+    int getMaximumGold(vector<vector<int>> &grid)
+    {
+        m = grid.size(), n = grid[0].size();
+        int ret = 0;
+
+        for (int i = 0; i < m; i++)
+            for (int j = 0; j < n; j++)
+            {
+                if (grid[i][j])
+                {
+                    vis[i][j] = true;
+                    ret = max(ret, dfs(grid, i, j) + grid[i][j]);
+                    vis[i][j] = false;
+                }
+            }
+
+        return ret;
+    }
+
+    int dfs(vector<vector<int>> &grid, int i, int j)
+    {
+        int ret = 0;
+        for (int k = 0; k < 4; k++)
+        {
+            int x = i + dx[k], y = j + dy[k];
+            if (x >= 0 && x < m && y >= 0 && y < n && !vis[x][y] && grid[x][y])
+            {
+                vis[x][y] = true;
+                ret = max(ret, dfs(grid, x, y) + grid[x][y]);
+                vis[x][y] = false;
+            }
+        }
+
+        return ret;
+    }
+
+    // 5
+    int lastStoneWeightII(vector<int> &stones)
+    {
+        int n = stones.size(), sum = 0;
+        for (auto w : stones)
+            sum += w;
+
+        int m = sum / 2;
+        vector<vector<int>> dp(n + 1, vector<int>(m + 1));
+        for (int i = 1; i <= n; i++)
+            for (int j = 0; j <= m; j++)
+            {
+                dp[i][j] = dp[i - 1][j];
+
+                if (j >= stones[i - 1])
+                    dp[i][j] = max(dp[i][j], dp[i - 1][j - stones[i - 1]] + stones[i - 1]);
+            }
+
+        return sum - 2 * dp[n][m];
+    }
+};
