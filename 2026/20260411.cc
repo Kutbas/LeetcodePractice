4ff9b04a5337db027eaa34da88c27524dcef3719
Kevin's Mac <kutbass@163.com>
2026-04-12 01:36:23 +0800

    20260411

---
+++ 2026/20260411.cc
@@ -0,0 +1,204 @@
+#include <bits/stdc++.h>
+#include <mutex>
+using namespace std;
+
+struct ListNode
+{
+    int val;
+    ListNode *next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
+
+struct TreeNode
+{
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+class Solution
+{
+public:
+    // 1
+    vector<int> smallestK(vector<int> &arr, int k)
+    {
+        srand(time(nullptr));
+        quickSort(arr, 0, arr.size() - 1, k);
+        return {arr.begin(), arr.begin() + k};
+    }
+
+    int getRandom(vector<int> &nums, int left, int right)
+    {
+        int r = rand();
+        return nums[r % (right - left + 1) + left];
+    }
+
+    void quickSort(vector<int> &nums, int l, int r, int k)
+    {
+        if (l > r)
+            return;
+
+        int key = getRandom(nums, l, r);
+        int left = l - 1, right = r + 1, i = l;
+        while (i < right)
+        {
+            if (nums[i] < key)
+                swap(nums[++left], nums[i++]);
+            else if (nums[i] == key)
+                i++;
+            else
+                swap(nums[--right], nums[i]);
+        }
+
+        int lRange = left - l + 1, mRange = (right - 1) - (left + 1) + 1;
+        if (lRange >= k)
+            quickSort(nums, l, left, k);
+        else if (lRange + mRange >= k)
+            return;
+        else
+            quickSort(nums, right, r, k - lRange - mRange);
+    }
+
+    // 3
+    vector<vector<int>> ret;
+    vector<int> path;
+    bool vis[7] = {0};
+    vector<vector<int>> permute(vector<int> &nums)
+    {
+        dfs(nums);
+        return ret;
+    }
+
+    void dfs(vector<int> &nums)
+    {
+        if (path.size() == nums.size())
+        {
+            ret.push_back(path);
+            return;
+        }
+
+        for (int i = 0; i < nums.size(); i++)
+        {
+            if (vis[i])
+                continue;
+            else
+            {
+                vis[i] = true;
+                path.push_back(nums[i]);
+                dfs(nums);
+                path.pop_back();
+                vis[i] = false;
+            }
+        }
+    }
+
+    // 4
+    vector<vector<int>> levelOrder(TreeNode *root)
+    {
+        vector<vector<int>> ret;
+        if (root == nullptr)
+            return ret;
+
+        queue<TreeNode *> q;
+        q.push(root);
+        while (q.size())
+        {
+            int sz = q.size();
+            vector<int> tmp;
+            while (sz--)
+            {
+                auto t = q.front();
+                q.pop();
+                tmp.push_back(t->val);
+
+                if (t->left)
+                    q.push(t->left);
+                if (t->right)
+                    q.push(t->right);
+            }
+
+            ret.push_back(tmp);
+        }
+
+        return ret;
+    }
+
+    // 5
+    int findSubstringInWraproundString(string s)
+    {
+        int n = s.size();
+        vector<int> dp(n, 1);
+
+        for (int i = 1; i < n; i++)
+            if (s[i - 1] + 1 == s[i] || (s[i - 1] == 'z' && s[i] == 'a'))
+                dp[i] += dp[i - 1];
+
+        int hash[26] = {0};
+        for (int i = 0; i < n; i++)
+        {
+            int index = s[i] - 'a';
+            hash[index] = max(hash[index], dp[i]);
+        }
+
+        int ret = 0;
+        for (int i = 0; i < 26; i++)
+            ret += hash[i];
+
+        return ret;
+    }
+
+};
+
+// 2
+class LRUCache
+{
+public:
+    LRUCache(int capacity) : _capacity(capacity)
+    {
+    }
+
+    int get(int key)
+    {
+        auto ret = _hash.find(key);
+        if (ret != _hash.end())
+        {
+            auto it = ret->second;
+            _list.splice(_list.begin(), _list, it);
+            return it->second;
+        }
+        return -1;
+    }
+
+    void put(int key, int value)
+    {
+        auto ret = _hash.find(key);
+        if (ret != _hash.end())
+        {
+            auto it = ret->second;
+            it->second = value;
+            _list.splice(_list.begin(), _list, it);
+        }
+        else
+        {
+            if (_capacity == _hash.size())
+            {
+                pair<int, int> back = _list.back();
+                _list.pop_back();
+                _hash.erase(back.first);
+            }
+            _list.push_front({key, value});
+            _hash[key] = _list.begin();
+        }
+    }
+
+private:
+    typedef list<pair<int, int>>::iterator LstItor;
+    int _capacity;
+    list<pair<int, int>> _list;
+    unordered_map<int, LstItor> _hash;
+};
