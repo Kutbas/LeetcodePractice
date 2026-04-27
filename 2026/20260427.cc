078962e914cba0d0c91c0628b3806c77e9138c8c
Kevin's Mac <kutbass@163.com>
2026-04-27 11:34:17 +0800

    20260427

---
+++ 2026/20260427.cc
@@ -0,0 +1,178 @@
+#include <bits/stdc++.h>
+#include <mutex>
+using namespace std;
+
+struct ListNode
+{
+    int val;
+    ListNode *next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
+
+struct TreeNode
+{
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+class Solution
+{
+public:
+    // 1
+    vector<int> intersection(vector<int> &nums1, vector<int> &nums2)
+    {
+        int hash[1001] = {0};
+        vector<int> ret;
+
+        for (auto n : nums1)
+            if (hash[n] == 0)
+                hash[n]++;
+
+        for (auto n : nums2)
+            if (--hash[n] == 0)
+                ret.push_back(n);
+
+        return ret;
+    }
+
+    // 2
+    int m, n;
+    int calculateMinimumHP(vector<vector<int>> &dungeon)
+    {
+        m = dungeon.size(), n = dungeon[0].size();
+        vector<vector<int>> dp(m + 1, vector<int>(n + 1, INT_MAX));
+        dp[m][n - 1] = dp[m - 1][n] = 1;
+
+        for (int i = m - 1; i >= 0; i--)
+            for (int j = n - 1; j >= 0; j--)
+            {
+                int prev = min(dp[i + 1][j], dp[i][j + 1]);
+                dp[i][j] = prev - dungeon[i][j];
+                dp[i][j] = max(1, dp[i][j]);
+            }
+
+        return dp[0][0];
+    }
+
+    // 3
+    void reorderList(ListNode *head)
+    {
+        if (head == nullptr || head->next == nullptr)
+            return;
+
+        ListNode *slow = head, *fast = head;
+        while (fast && fast->next)
+        {
+            slow = slow->next;
+            fast = fast->next->next;
+        }
+
+        ListNode *head2 = new ListNode(0);
+        ListNode *cur = slow->next;
+        slow->next = nullptr;
+
+        while (cur)
+        {
+            ListNode *next = cur->next;
+            cur->next = head2->next;
+            head2->next = cur;
+            cur = next;
+        }
+
+        ListNode *ret = new ListNode(0);
+        ListNode *prev = ret;
+        ListNode *cur1 = head, *cur2 = head2->next;
+        while (cur1 || cur2)
+        {
+            if (cur1)
+            {
+                prev->next = cur1;
+                prev = prev->next;
+                cur1 = cur1->next;
+            }
+            if (cur2)
+            {
+                prev->next = cur2;
+                prev = prev->next;
+                cur2 = cur2->next;
+            }
+        }
+
+        prev = ret->next;
+        delete ret, cur, cur1, cur2;
+        return;
+    }
+
+    // 4
+    ListNode *swapPairs(ListNode *head)
+    {
+        if (head == nullptr || head->next == nullptr)
+            return head;
+
+        ListNode *ret = head->next;
+        ListNode *tmp = swapPairs(head->next->next);
+        ret->next = head;
+        head->next = tmp;
+
+        return ret;
+    }
+
+    // 5
+    int findLength(vector<int> &nums1, vector<int> &nums2)
+    {
+        int m = nums1.size(), n = nums2.size(), ret = 0;
+        vector<vector<int>> dp(m + 1, vector<int>(n + 1));
+
+        for (int i = 1; i <= m; i++)
+            for (int j = 1; j <= n; j++)
+            {
+                if (nums1[i - 1] == nums2[j - 1])
+                    dp[i][j] = dp[i - 1][j - 1] + 1;
+                ret = max(ret, dp[i][j]);
+            }
+
+        return ret;
+    }
+
+    // 6
+    int maxSubArray(vector<int> &nums)
+    {
+        int n = nums.size(), ret = INT_MIN;
+        vector<int> dp(n + 1);
+
+        for (int i = 1; i <= n; i++)
+        {
+            dp[i] = max(dp[i - 1] + nums[i - 1], nums[i - 1]);
+            ret = max(ret, dp[i]);
+        }
+
+        return ret;
+    }
+
+    // 7
+    int numDistinct(string s, string t)
+    {
+        int m = t.size(), n = s.size();
+        s = " " + s, t = " " + t;
+        vector<vector<double>> dp(m + 1, vector<double>(n + 1));
+
+        for (int i = 0; i <= n; i++)
+            dp[0][i] = 1;
+
+        for (int i = 1; i <= m; i++)
+            for (int j = 1; j <= n; j++)
+            {
+                dp[i][j] = dp[i][j - 1];
+                if (t[i] == s[j])
+                    dp[i][j] += dp[i - 1][j - 1];
+            }
+
+        return dp[m][n];
+    }
+};
