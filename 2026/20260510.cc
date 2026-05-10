7bda2a686961a5f45b1653d48608722fd7f9c568
Kevin's Mac <kutbass@163.com>
2026-05-10 15:28:30 +0800

    20260510

---
+++ 2026/20260510.cc
@@ -0,0 +1,247 @@
+#include <bits/stdc++.h>
+#include <mutex>
+using namespace std;
+
+struct ListNode
+{
+    int val;
+    ListNode *next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
+
+struct TreeNode
+{
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+class Solution
+{
+public:
+    // 1
+    int tmp[50010];
+    int reversePairs(vector<int> &nums)
+    {
+        return mergeSort(nums, 0, nums.size() - 1);
+    }
+
+    int mergeSort(vector<int> &nums, int left, int right)
+    {
+        if (left >= right)
+            return 0;
+
+        int mid = left + (right - left) / 2, ret = 0;
+        ret += mergeSort(nums, left, mid);
+        ret += mergeSort(nums, mid + 1, right);
+
+        int i = 0, cur1 = left, cur2 = mid + 1;
+        while (cur1 <= mid && cur2 <= right)
+        {
+            if (cur1 <= mid && nums[cur1] / 2.0 <= nums[cur2])
+            {
+                cur2++;
+            }
+            else
+            {
+                ret += right - cur2 + 1;
+                cur1++;
+            }
+        }
+
+        cur1 = left, cur2 = mid + 1;
+        while (cur1 <= mid && cur2 <= right)
+            tmp[i++] = nums[cur1] <= nums[cur2] ? nums[cur2++] : nums[cur1++];
+        while (cur1 <= mid)
+            tmp[i++] = nums[cur1++];
+        while (cur2 <= right)
+            tmp[i++] = nums[cur2++];
+
+        for (int j = left; j <= right; j++)
+            nums[j] = tmp[j - left];
+
+        return ret;
+    }
+
+    // 3
+    int canCompleteCircuit(vector<int> &gas, vector<int> &cost)
+    {
+        int n = gas.size();
+
+        for (int i = 0; i < n; i++)
+        {
+            int step = 0, rest = 0;
+            for (; step < n; step++)
+            {
+                int index = (i + step) % n;
+                rest += gas[index] - cost[index];
+                if (rest < 0)
+                    break;
+            }
+
+            if (rest >= 0)
+                return i;
+
+            i = i + step;
+        }
+
+        return -1;
+    }
+
+    // 4
+    int maxProfit(vector<int> &prices, int fee)
+    {
+        int n = prices.size();
+        vector<int> f(n);
+        auto g = f;
+
+        f[0] = -prices[0];
+        for (int i = 1; i < n; i++)
+        {
+            f[i] = max(f[i - 1], g[i - 1] - prices[i]);
+            g[i] = max(g[i - 1], f[i - 1] + prices[i] - fee);
+        }
+
+        return g[n - 1];
+    }
+
+    // 5
+    int countSubstrings(string s)
+    {
+        int n = s.size(), ret = 0;
+
+        vector<vector<bool>> dp(n, vector<bool>(n));
+        for (int i = n - 1; i >= 0; i--)
+            for (int j = i; j < n; j++)
+            {
+                if (s[i] == s[j])
+                    dp[i][j] = j - i + 1 > 3 ? dp[i + 1][j - 1] : true;
+                if (dp[i][j])
+                    ret++;
+            }
+
+        return ret;
+    }
+
+    // 6
+    int longestPalindromeSubseq(string s)
+    {
+        int n = s.size();
+        vector<vector<int>> dp(n, vector<int>(n));
+
+        for (int i = n - 1; i >= 0; i--)
+            for (int j = i; j < n; j++)
+            {
+                if (s[i] == s[j])
+                    dp[i][j] = i == j ? 1 : dp[i + 1][j - 1] + 2;
+                else
+                    dp[i][j] = max(dp[i + 1][j], dp[i][j - 1]);
+            }
+
+        return dp[0][n - 1];
+    }
+
+    // 7
+    int numDistinct(string s, string t)
+    {
+        int m = t.size(), n = s.size();
+        s = " " + s, t = " " + t;
+        vector<vector<double>> dp(m + 1, vector<double>(n + 1));
+
+        for (int i = 0; i <= n; i++)
+            dp[0][i] = 1;
+
+        for (int i = 1; i <= m; i++)
+            for (int j = 1; j <= n; j++)
+            {
+                dp[i][j] = dp[i][j - 1];
+
+                if (t[i] == s[j])
+                    dp[i][j] += dp[i - 1][j - 1];
+            }
+
+        return dp[m][n];
+    }
+
+    // 8
+    bool isMatch(string s, string p)
+    {
+        int m = s.size(), n = p.size();
+        s = " " + s, p = " " + p;
+        vector<vector<bool>> dp(m + 1, vector<bool>(n + 1));
+        dp[0][0] = true;
+
+        for (int i = 1; i <= n; i++)
+            if (p[i] == '*')
+                dp[0][i] = true;
+            else
+                break;
+
+        for (int i = 1; i <= m; i++)
+            for (int j = 1; j <= n; j++)
+            {
+                if (p[j] == '*')
+                {
+                    dp[i][j] = dp[i - 1][j] || dp[i][j - 1];
+                }
+                else
+                    dp[i][j] = (p[j] == '?' || p[j] == s[i]) && dp[i - 1][j - 1];
+            }
+
+        return dp[m][n];
+    }
+};
+
+// 2
+class LRUCache
+{
+public:
+    LRUCache(int capacity) : _capacity(capacity)
+    {
+    }
+
+    int get(int key)
+    {
+        auto ret = _hash.find(key);
+        if (ret != _hash.end())
+        {
+            auto it = ret->second;
+            _list.splice(_list.begin(), _list, it);
+            return it->second;
+        }
+        return -1;
+    }
+
+    void put(int key, int value)
+    {
+        auto ret = _hash.find(key);
+        if (ret != _hash.end())
+        {
+            auto it = ret->second;
+            it->second = value;
+            _list.splice(_list.begin(), _list, it);
+        }
+        else
+        {
+            if (_capacity == _hash.size())
+            {
+                pair<int, int> back = _list.back();
+                _hash.erase(back.first);
+                _list.pop_back();
+            }
+            _list.push_front({key, value});
+            _hash[key] = _list.begin();
+        }
+    }
+
+private:
+    typedef list<pair<int, int>>::iterator LstItor;
+    int _capacity;
+    list<pair<int, int>> _list;
+    unordered_map<int, LstItor> _hash;
+};
