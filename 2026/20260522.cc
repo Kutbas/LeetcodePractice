baf0463a1c4578bb8dfdfc5401ead0b4ed78a66c
Kevin's Mac <kutbass@163.com>
2026-05-22 11:13:59 +0800

    20260522

---
+++ 2026/20260522.cc
@@ -0,0 +1,129 @@
+#include <bits/stdc++.h>
+#include <mutex>
+using namespace std;
+
+struct ListNode
+{
+    int val;
+    ListNode *next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
+
+struct TreeNode
+{
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+class Solution
+{
+public:
+    // 1
+    int integerReplacement(int n)
+    {
+        int ret = 0;
+
+        while (n != 1)
+        {
+            if (n % 2 == 0)
+            {
+                n /= 2;
+                ret++;
+            }
+            else if (n == 3)
+            {
+                n = 1;
+                ret += 2;
+            }
+            else if (n % 4 == 1)
+            {
+                n /= 2;
+                ret += 2;
+            }
+            else
+            {
+                n = n / 2 + 1;
+                ret += 2;
+            }
+        }
+
+        return ret;
+    }
+
+    // 2
+    int numDistinct(string s, string t)
+    {
+        int m = t.size(), n = s.size();
+        s = " " + s, t = " " + t;
+
+        vector<vector<double>> dp(m + 1, vector<double>(n + 1));
+        for (int i = 0; i <= n; i++)
+            dp[0][i] = 1;
+
+        for (int i = 1; i <= m; i++)
+            for (int j = 1; j <= n; j++)
+            {
+                dp[i][j] = dp[i][j - 1];
+                if (t[i] == s[j])
+                    dp[i][j] += dp[i - 1][j - 1];
+            }
+
+        return dp[m][n];
+    }
+
+    // 3
+    bool isMatch(string s, string p)
+    {
+        int m = s.size(), n = p.size();
+        s = " " + s, p = " " + p;
+
+        vector<vector<bool>> dp(m + 1, vector<bool>(n + 1));
+        dp[0][0] = true;
+        for (int i = 2; i <= n; i += 2)
+            if (p[i] == '*')
+                dp[0][i] = true;
+            else
+                break;
+
+        for (int i = 1; i <= m; i++)
+            for (int j = 1; j <= n; j++)
+            {
+                if (p[j] == '*')
+                {
+                    bool case1 = dp[i][j - 2];
+                    bool case2 = (p[j - 1] == '.' || p[j - 1] == s[i]) && dp[i - 1][j];
+                    dp[i][j] = case1 || case2;
+                }
+                else
+                    dp[i][j] = (p[j] == '.' || p[j] == s[i]) && dp[i - 1][j - 1];
+            }
+
+        return dp[m][n];
+    }
+
+    // 4
+    int numSquares(int n)
+    {
+        int m = sqrt(n);
+        vector<vector<int>> dp(m + 1, vector<int>(n + 1));
+
+        for (int i = 1; i <= n; i++)
+            dp[0][i] = INT_MAX;
+
+        for (int i = 1; i <= m; i++)
+            for (int j = 1; j <= n; j++)
+            {
+                dp[i][j] = dp[i - 1][j];
+                if (j >= i * i)
+                    dp[i][j] = min(dp[i][j], dp[i][j - i * i] + 1);
+            }
+
+        return dp[m][n];
+    }
+};
