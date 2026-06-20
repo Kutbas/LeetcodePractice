893ce6f7428220bac2700b70f59d4506b890d358
Kevin's Mac <kutbass@163.com>
2026-06-20 17:49:55 +0800

    20260620

---
+++ 2026/20260620.cc
@@ -0,0 +1,228 @@
+#include <mutex>
+#include <vector>
+#include <algorithm>
+#include <climits>
+#include <list>
+#include <unordered_map>
+#include <unordered_set>
+#include <queue>
+using namespace std;
+
+struct ListNode
+{
+    int val;
+    ListNode *next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
+
+struct TreeNode
+{
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+class Solution
+{
+public:
+    // 1
+    ListNode *reverseList(ListNode *head)
+    {
+        if (head == nullptr || head->next == nullptr)
+            return head;
+
+        ListNode *newhead = reverseList(head->next);
+        head->next->next = head;
+        head->next = nullptr;
+
+        return newhead;
+    }
+
+    // 2
+    int memo[101][101] = {0};
+    int uniquePaths(int m, int n)
+    {
+        return dfs(m, n);
+    }
+
+    int dfs(int m, int n)
+    {
+        if (m == 0 || n == 0)
+            return 0;
+
+        if (m == 1 && n == 1)
+            return 1;
+
+        if (memo[m][n])
+            return memo[m][n];
+
+        int ret = dfs(m - 1, n) + dfs(m, n - 1);
+        memo[m][n] = ret;
+
+        return ret;
+    }
+
+    // 3
+    int m, n;
+    int dx[4] = {0, 0, 1, -1};
+    int dy[4] = {1, -1, 0, 0};
+    bool vis[101][101] = {0};
+    int getMaximumGold(vector<vector<int>> &grid)
+    {
+        m = grid.size(), n = grid[0].size();
+
+        int ret = 0;
+        for (int i = 0; i < m; i++)
+            for (int j = 0; j < n; j++)
+                if (grid[i][j])
+                {
+                    vis[i][j] = true;
+                    ret = max(ret, grid[i][j] + dfs(grid, i, j));
+                    vis[i][j] = false;
+                }
+
+        return ret;
+    }
+
+    int dfs(vector<vector<int>> &grid, int i, int j)
+    {
+
+        int ret = 0;
+        for (int k = 0; k < 4; k++)
+        {
+            int x = i + dx[k], y = j + dy[k];
+            if (x >= 0 && x < m && y >= 0 && y < n && grid[x][y] && !vis[x][y])
+            {
+                vis[x][y] = true;
+                ret = max(ret, grid[x][y] + dfs(grid, x, y));
+                vis[x][y] = false;
+            }
+        }
+
+        return ret;
+    }
+
+    // 4
+    int m, n;
+    int dx[4] = {0, 0, 1, -1};
+    int dy[4] = {1, -1, 0, 0};
+    vector<vector<int>> pacificAtlantic(vector<vector<int>> &heights)
+    {
+        m = heights.size(), n = heights[0].size();
+        vector<vector<bool>> pac(m, vector<bool>(n));
+        vector<vector<bool>> atl(m, vector<bool>(n));
+
+        for (int i = 0; i < m; i++)
+        {
+            dfs(heights, i, 0, pac);
+            dfs(heights, i, n - 1, atl);
+        }
+
+        for (int i = 0; i < n; i++)
+        {
+            dfs(heights, 0, i, pac);
+            dfs(heights, m - 1, i, atl);
+        }
+
+        vector<vector<int>> ret;
+        for (int i = 0; i < m; i++)
+            for (int j = 0; j < n; j++)
+                if (pac[i][j] && atl[i][j])
+                    ret.push_back({i, j});
+
+        return ret;
+    }
+
+    void dfs(vector<vector<int>> &heights, int i, int j, vector<vector<bool>> &vis)
+    {
+        vis[i][j] = true;
+
+        for (int k = 0; k < 4; k++)
+        {
+            int x = i + dx[k], y = j + dy[k];
+            if (x >= 0 && x < m && y >= 0 && y < n && !vis[x][y] && heights[x][y] >= heights[i][j])
+                dfs(heights, x, y, vis);
+        }
+    }
+
+    // 5
+    int longestPalindrome(string s)
+    {
+        int hash[128] = {0};
+
+        for (char ch : s)
+            hash[ch]++;
+
+        int ret = 0;
+
+        for (int i : hash)
+            ret += i / 2 * 2;
+
+        return ret < s.size() ? ret + 1 : ret;
+    }
+
+    // 6
+    string optimalDivision(vector<int> &nums)
+    {
+        int n = nums.size();
+        if (n == 1)
+            return to_string(nums[0]);
+
+        if (n == 2)
+            return to_string(nums[0]) + '/' + to_string(nums[1]);
+
+        string ret = to_string(nums[0]) + "/(" + to_string(nums[1]);
+        for (int i = 2; i < n; i++)
+            ret += "/" + to_string(nums[i]);
+
+        ret += ")";
+
+        return ret;
+    }
+
+    // 7
+    int numDistinct(string s, string t)
+    {
+        int m = t.size(), n = s.size();
+        s = " " + s, t = " " + t;
+
+        vector<vector<double>> dp(m + 1, vector<double>(n + 1));
+        for (int i = 0; i <= n; i++)
+            dp[0][i] = 1;
+
+        for (int i = 1; i <= m; i++)
+            for (int j = 1; j <= n; j++)
+            {
+                dp[i][j] = dp[i][j - 1];
+                if (t[i] == s[j])
+                    dp[i][j] += dp[i - 1][j - 1];
+            }
+
+        return dp[m][n];
+    }
+
+    // 8
+    int m, n;
+    int dx[4] = {0, 0, 1, -1};
+    int dy[4] = {1, -1, 0, 0};
+    int minPathSum(vector<vector<int>> &grid)
+    {
+        m = grid.size(), n = grid[0].size();
+
+        vector<vector<int>> dp(m + 1, vector<int>(n + 1, INT_MAX));
+        dp[0][1] = dp[1][0] = 0;
+        for (int i = 1; i <= m; i++)
+            for (int j = 1; j <= n; j++)
+            {
+                int prev = min(dp[i - 1][j], dp[i][j - 1]);
+                dp[i][j] = prev + grid[i - 1][j - 1];
+            }
+
+        return dp[m][n];
+    }
+};
