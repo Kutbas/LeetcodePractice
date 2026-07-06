e6ff3c1bce73e87a9d7387544e939405852d1dbf
Kevin's Mac <kutbass@163.com>
2026-07-06 22:01:20 +0800

    20260706

---
+++ 2026/20260706.cc
@@ -0,0 +1,291 @@
+#include <mutex>
+#include <vector>
+#include <algorithm>
+#include <climits>
+#include <list>
+#include <unordered_map>
+#include <unordered_set>
+#include <queue>
+#include <stack>
+#include <iostream>
+#include <regex>
+#include <map>
+using namespace std;
+
+struct ListNode
+{
+    int val;
+    ListNode *next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
+
+struct TreeNode
+{
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+class Solution
+{
+public:
+    // 1
+    vector<int> ret;
+    vector<int> index;
+    vector<int> tmp;
+    vector<int> tmpIndex;
+    vector<int> countSmaller(vector<int> &nums)
+    {
+        int n = nums.size();
+        ret.resize(n);
+        index.resize(n);
+        tmp.resize(n);
+        tmpIndex.resize(n);
+
+        for (int i = 0; i < n; i++)
+            index[i] = i;
+
+        mergeSort(nums, 0, nums.size() - 1);
+
+        return ret;
+    }
+
+    void mergeSort(vector<int> &nums, int left, int right)
+    {
+        if (left >= right)
+            return;
+
+        int mid = left + (right - left) / 2;
+        mergeSort(nums, left, mid);
+        mergeSort(nums, mid + 1, right);
+
+        int i = 0, cur1 = left, cur2 = mid + 1;
+        while (cur1 <= mid && cur2 <= right)
+        {
+            if (nums[cur1] <= nums[cur2])
+            {
+                tmp[i] = nums[cur2];
+                tmpIndex[i++] = index[cur2++];
+            }
+            else
+            {
+                ret[index[cur1]] += right - cur2 + 1;
+                tmp[i] = nums[cur1];
+                tmpIndex[i++] = index[cur1++];
+            }
+        }
+        while (cur1 <= mid)
+        {
+            tmp[i] = nums[cur1];
+            tmpIndex[i++] = index[cur1++];
+        }
+        while (cur2 <= right)
+        {
+            tmp[i] = nums[cur2];
+            tmpIndex[i++] = index[cur2++];
+        }
+
+        for (int j = left; j <= right; j++)
+        {
+            nums[j] = tmp[j - left];
+            index[j] = tmpIndex[j - left];
+        }
+    }
+
+    // 2
+    vector<vector<int>> merge(vector<vector<int>> &intervals)
+    {
+        sort(intervals.begin(), intervals.end());
+        vector<vector<int>> ret;
+
+        int n = intervals.size(), left = intervals[0][0],
+            right = intervals[0][1];
+        for (int i = 1; i < n; i++)
+        {
+            int a = intervals[i][0], b = intervals[i][1];
+            if (a <= right)
+                right = max(right, b);
+            else
+            {
+                ret.push_back({left, right});
+                left = a, right = b;
+            }
+        }
+
+        ret.push_back({left, right});
+
+        return ret;
+    }
+
+    // 3
+    int deleteAndEarn(vector<int> &nums)
+    {
+        const int N = 10001;
+        int arr[N] = {0};
+        for (auto n : nums)
+            arr[n] += n;
+
+        vector<int> f(N), g(N);
+        for (int i = 1; i < N; i++)
+        {
+            f[i] = g[i - 1] + arr[i];
+            g[i] = max(g[i - 1], f[i - 1]);
+        }
+
+        return max(f[N - 1], g[N - 1]);
+    }
+
+    // 4
+    bool checkPartitioning(string s)
+    {
+        int n = s.size();
+        vector<vector<bool>> dp(n, vector<bool>(n));
+
+        for (int i = n - 1; i >= 0; i--)
+            for (int j = i; j < n; j++)
+                if (s[i] == s[j])
+                    dp[i][j] = j - i + 1 > 3 ? dp[i + 1][j - 1] : true;
+
+        for (int i = 1; i < n - 1; i++)
+            for (int j = i; j < n - 1; j++)
+                if (dp[0][i - 1] && dp[i][j] && dp[j + 1][n - 1])
+                    return true;
+
+        return false;
+    }
+
+    // 5
+    bool isMatch(string s, string p)
+    {
+        int m = s.size(), n = p.size();
+        s = " " + s, p = " " + p;
+        vector<vector<bool>> dp(m + 1, vector<bool>(n + 1));
+        dp[0][0] = true;
+
+        for (int i = 1; i <= n; i++)
+            if (p[i] == '*')
+                dp[0][i] = true;
+            else
+                break;
+
+        for (int i = 1; i <= m; i++)
+            for (int j = 1; j <= n; j++)
+            {
+                if (p[j] == '*')
+                {
+                    // bool case1=dp[i-1][j];
+                    // bool case2=(p[j-1]=='?'||p[j-1]==s[i])&&dp[i-1][j];
+                    dp[i][j] = dp[i - 1][j] || dp[i][j - 1];
+                }
+                else
+                    dp[i][j] = (p[j] == '?' || p[j] == s[i]) && dp[i - 1][j - 1];
+            }
+
+        return dp[m][n];
+    }
+
+    // 6
+    int findLength(vector<int> &nums1, vector<int> &nums2)
+    {
+        int m = nums1.size(), n = nums2.size();
+        vector<vector<int>> dp(m + 1, vector<int>(n + 1));
+
+        int ret = 0;
+        for (int i = 1; i <= m; i++)
+            for (int j = 1; j <= n; j++)
+            {
+                if (nums1[i - 1] == nums2[j - 1])
+                    dp[i][j] = dp[i - 1][j - 1] + 1;
+                // else
+                //     dp[i][j]=max(dp[i-1][j],dp[i][j-1]);
+                ret = max(ret, dp[i][j]);
+            }
+
+        return ret;
+    }
+
+    // 7
+    bool isPalindrome(string s)
+    {
+        string tmp;
+        for (char ch : s)
+            if (isalnum(ch))
+                tmp += tolower(ch);
+
+        int left = 0, right = tmp.size() - 1;
+        while (left < right)
+            if (tmp[left] != tmp[right])
+                return false;
+            else
+                left++, right--;
+
+        return true;
+    }
+
+    // 9
+    TreeNode *buildTree(vector<int> &inorder, vector<int> &postorder)
+    {
+        int i = postorder.size() - 1;
+        return _buildTree(inorder, postorder, i, 0, inorder.size() - 1);
+    }
+
+    TreeNode *_buildTree(vector<int> &inorder, vector<int> &postorder, int &posti, int inBegin, int inEnd)
+    {
+        if (inBegin > inEnd)
+            return nullptr;
+
+        TreeNode *root = new TreeNode(postorder[posti]);
+
+        int rooti = inBegin;
+        while (rooti <= inEnd)
+            if (inorder[rooti] == postorder[posti])
+                break;
+            else
+                rooti++;
+
+        --posti;
+        root->right = _buildTree(inorder, postorder, posti, rooti + 1, inEnd);
+        root->left = _buildTree(inorder, postorder, posti, inBegin, rooti - 1);
+
+        return root;
+    }
+};
+
+// 8
+string s;
+
+string change(string &s)
+{
+    if (s[0] >= 'A' && s[0] <= 'Z')
+        s[0] = tolower(s[0]);
+
+    return s;
+}
+
+int main()
+{
+    getline(cin, s);
+
+    regex re{R"([a-zA-Z]+)"};
+    sregex_token_iterator begin(s.begin(), s.end(), re, 0);
+    sregex_token_iterator end;
+
+    vector<string> words(begin, end);
+    map<string, int> hash;
+
+    for (auto &word : words)
+    {
+        string tmp = change(word);
+        hash[tmp]++;
+    }
+
+    for (auto &[a, b] : hash)
+        cout << a << ":" << b << endl;
+
+    return 0;
+}
