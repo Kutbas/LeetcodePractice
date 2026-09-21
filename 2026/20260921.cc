29dee52c4e9b3c7b73c22965a3e7432b98b08a98
Kevin's Mac <kutbass@163.com>
2026-09-21 11:30:58 +0800

    20260921

---
+++ 2026/20260921.cc
@@ -0,0 +1,124 @@
+#include <mutex>
+#include <vector>
+#include <algorithm>
+#include <climits>
+#include <list>
+#include <unordered_map>
+#include <unordered_set>
+#include <queue>
+#include <stack>
+#include <iostream>
+#include <sstream>
+#include <regex>
+#include <map>
+using namespace std;
+
+struct ListNode
+{
+    int val;
+    ListNode *next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
+
+struct TreeNode
+{
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+class Solution
+{
+public:
+    // 1
+    int longestPalindrome(string s)
+    {
+        int n = s.size(), ret = 0;
+        int hash[128] = {0};
+
+        for (char ch : s)
+            hash[ch]++;
+
+        for (int i : hash)
+            ret += i / 2 * 2;
+
+        return ret < n ? ret + 1 : ret;
+    }
+
+    // 2
+    int m, n;
+    int uniquePathsWithObstacles(vector<vector<int>> &obstacleGrid)
+    {
+        m = obstacleGrid.size(), n = obstacleGrid[0].size();
+        vector<vector<int>> dp(m + 1, vector<int>(n + 1));
+        dp[0][1] = 1;
+
+        for (int i = 1; i <= m; i++)
+            for (int j = 1; j <= n; j++)
+            {
+                if (obstacleGrid[i - 1][j - 1] == 0)
+                    dp[i][j] = dp[i - 1][j] + dp[i][j - 1];
+            }
+
+        return dp[m][n];
+    }
+
+    // 3
+    int minCut(string s)
+    {
+        int n = s.size();
+        vector<vector<bool>> isPal(n, vector<bool>(n));
+
+        for (int i = n - 1; i >= 0; i--)
+            for (int j = i; j < n; j++)
+                if (s[i] == s[j])
+                    isPal[i][j] = j - i + 1 > 3 ? isPal[i + 1][j - 1] : true;
+
+        vector<int> dp(n, INT_MAX);
+        for (int i = 0; i < n; i++)
+        {
+            if (isPal[0][i])
+                dp[i] = 0;
+            else
+            {
+                for (int j = 1; j <= i; j++)
+                {
+                    if (isPal[j][i])
+                        dp[i] = min(dp[i], dp[j - 1] + 1);
+                }
+            }
+        }
+
+        return dp[n - 1];
+    }
+
+    // 4
+    int FindGreatestSumOfSubArray(vector<int> &array)
+    {
+        int n = array.size(), ret = INT_MIN;
+        vector<int> dp(n + 1);
+
+        for (int i = 1; i <= n; i++)
+        {
+            dp[i] = max(dp[i - 1] + array[i - 1], array[i - 1]);
+            ret = max(ret, dp[i]);
+        }
+
+        return ret;
+    }
+};
+
+// 5
+int main()
+{
+    string s;
+    while (cin >> s)
+        ;
+    cout << s.size() << endl;
+    return 0;
+}
